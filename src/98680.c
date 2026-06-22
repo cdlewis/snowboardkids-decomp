dbd03fe8d187a094ddf26e4df89bd9cd0c29acb9
Chris Lewis <me@chrislewis.au>
2026-06-22 17:59:25 -0400

    task-runner(decompile-unmatched): fix 'func_800984CC'

--- src/98680.c
+++ src/98680.c
@@ -30,7 +30,22 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/98680/func_80098458.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/98680/func_800984CC.s")
+extern void func_80097BAC(void *, s16);
+extern void func_80097C18(void *, s16);
+extern void func_80097C84(void *, s16);
+extern void func_80097CF0(void *, void *, void *);
+
+void func_800984CC(void *arg0, s16 arg1, s16 arg2, s16 arg3) {
+    s16 sp58[0x10];
+    s16 sp38[0x10];
+    s16 sp18[0x10];
+
+    func_80097BAC(sp58, arg1);
+    func_80097C84(sp38, arg3);
+    func_80097CF0(sp58, sp38, sp18);
+    func_80097C18(sp38, arg2);
+    func_80097CF0(sp18, sp38, arg0);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/98680/func_8009853C.s")
 
