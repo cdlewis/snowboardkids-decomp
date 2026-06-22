56ee859850bc0375ca278b5670de14986fa0efff
Chris Lewis <me@chrislewis.au>
2026-06-22 15:49:27 -0400

    task-runner(decompile-unmatched): fix 'func_800363B4'

--- src/36F80.c
+++ src/36F80.c
@@ -1,11 +1,30 @@
 #include "common.h"
 
+extern void func_80036388(void);
+extern void func_800373AC(s32 *arg0);
+extern void func_80041DD4(s32 arg0, s32 arg1);
+extern s32 func_80041FB4(s32 arg0);
+extern void func_800428C8(s32 arg0);
+extern void func_80071824(s32 arg0, void (*arg1)(void));
+extern s32 D_8010B1C0;
+extern u16 D_8010B1A2;
+
 void func_80036380(void) {
 }
 
 #pragma GLOBAL_ASM("asm/nonmatchings/36F80/func_80036388.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/36F80/func_800363B4.s")
+void func_800363B4(s32 arg0);
+
+void func_800363B4(s32 arg0) {
+    func_80041FB4(4);
+    func_800428C8(4);
+    func_800373AC(&D_8010B1C0);
+    if (D_8010B1A2 == 0x41) {
+        func_80071824(arg0, func_80036388);
+        func_80041DD4(4, 0x61);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/36F80/func_80036418.s")
 
