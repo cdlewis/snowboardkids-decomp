395b44d5c3d4749d6d5f6608d309305c54443138
Chris Lewis <me@chrislewis.au>
2026-06-22 16:08:27 -0400

    task-runner(decompile-unmatched): fix 'func_800566C0'

--- src/562B0.c
+++ src/562B0.c
@@ -30,16 +30,29 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/562B0/func_8005663C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/562B0/func_800566C0.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/562B0/func_8005673C.s")
-
 extern void func_80056348(void *arg0, s32 arg1);
 extern void func_8005638C(void *arg0, s32 arg1);
 extern void func_800563C4(void *arg0, s32 arg1);
 extern void func_80056444(void *arg0, s32 arg1);
 extern void func_800564A8(void *arg0);
 extern void func_80071824(void *arg0, void (*arg1)(void));
+
+void func_8005663C(void);
+
+void func_800566C0(void *arg0) {
+    func_80056348(arg0, 0x130);
+    func_8005638C(arg0, -0x20000);
+    func_800563C4(arg0, 0x100000);
+    func_80056444(arg0, -3);
+    func_800564A8(arg0);
+    *(s16 *)((s32)arg0 + 0x3A) = *(s16 *)((s32)arg0 + 0x3A) - 1;
+    if (*(s16 *)((s32)arg0 + 0x3A) == 0) {
+        func_80071824(arg0, func_8005663C);
+    }
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/562B0/func_8005673C.s")
+
 extern void func_8005673C(void);
 
 void func_800567C4(void *arg0) {
