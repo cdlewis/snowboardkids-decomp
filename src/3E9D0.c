204e8a884915f8e739ce039a6b15e97b973f26ba
Chris Lewis <me@chrislewis.au>
2026-06-23 17:44:10 -0400

    task-runner(decompile-unmatched): fix 'func_8003E5A8'

--- src/3E9D0.c
+++ src/3E9D0.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+extern void func_80045914(void);
+extern void func_80099658(s32);
+extern void func_8009954C(s32);
+extern s8 D_800DEED4;
+extern u8 D_80123750;
+extern u8 D_80123751;
+
 #pragma GLOBAL_ASM("asm/nonmatchings/3E9D0/func_8003DDD0.s")
 
 struct ALSeqPlayer;
@@ -26,4 +33,12 @@ void func_8003DFB0(void) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/3E9D0/func_8003E514.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/3E9D0/func_8003E5A8.s")
+void func_8003E5A8(void) {
+    if (D_80123750 == 2) {
+        func_80045914();
+        D_80123751 = 0;
+        D_800DEED4 = 0;
+        func_80099658(2);
+        func_8009954C(4);
+    }
+}
