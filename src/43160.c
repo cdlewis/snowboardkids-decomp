fdf148b761f2abdf50119473f5e4f7ecc7a9926d
Chris Lewis <me@chrislewis.au>
2026-06-23 17:38:21 -0400

    task-runner(decompile-unmatched): fix 'func_800428C8'

--- src/43160.c
+++ src/43160.c
@@ -1,12 +1,25 @@
 #include "common.h"
 
+extern s32 func_80043040(s16);
+extern void func_8004215C(s32);
+extern void func_800483FC(void *, void *, s32);
+extern s16 D_8011218A[];
+extern s32 D_801248B0;
+extern void func_80042574(void);
+
 #pragma GLOBAL_ASM("asm/nonmatchings/43160/func_80042560.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/43160/func_80042574.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/43160/func_8004270C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/43160/func_800428C8.s")
+void func_800428C8(s32 arg0) {
+    s32 temp = func_80043040(D_8011218A[arg0]);
+
+    func_8004215C(temp);
+    *(s16 *)((u8 *)temp + 0x8) = 0;
+    func_800483FC(&D_801248B0, func_80042574, temp);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/43160/func_80042920.s")
 
