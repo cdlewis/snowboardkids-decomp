19bbf3181275578fb5fffd093eaf3222f63ef061
Chris Lewis <me@chrislewis.au>
2026-06-23 17:42:14 -0400

    task-runner(decompile-unmatched): fix 'func_8007A278'

--- src/79030.c
+++ src/79030.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+extern s32 func_80043040(s16);
+extern void func_80045A78(s32, s32, s32, s32);
+extern s16 D_8011216E;
+
 #pragma GLOBAL_ASM("asm/nonmatchings/79030/func_80078430.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/79030/func_80078568.s")
@@ -34,7 +38,9 @@ void func_80079750(s32 arg0) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/79030/func_8007A108.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/79030/func_8007A278.s")
+void func_8007A278(s32 arg0) {
+    func_80045A78(0xC, 0x2C, func_80043040(D_8011216E), 0x90);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/79030/func_8007A2B8.s")
 
