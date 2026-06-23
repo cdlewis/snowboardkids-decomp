94f8fcf77b4321f12d787e1d912a985225a29044
Chris Lewis <me@chrislewis.au>
2026-06-23 17:55:42 -0400

    task-runner(decompile-unmatched): fix 'func_8000DD74'

--- src/E3F0.c
+++ src/E3F0.c
@@ -1,8 +1,14 @@
 #include "common.h"
 
+extern void func_800483FC(void *, void *, s32);
+extern s32 D_80124868;
+extern void func_8000DF9C(void);
+
 #pragma GLOBAL_ASM("asm/nonmatchings/E3F0/func_8000D7F0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/E3F0/func_8000DD74.s")
+void func_8000DD74(s32 arg0) {
+    func_800483FC(&D_80124868, func_8000DD74, arg0);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/E3F0/func_8000DDA4.s")
 
@@ -16,7 +22,9 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/E3F0/func_8000E7CC.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/E3F0/func_8000E844.s")
+void func_8000E844(s32 arg0) {
+    func_800483FC(&D_80124868, func_8000DF9C, arg0);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/E3F0/func_8000E874.s")
 
