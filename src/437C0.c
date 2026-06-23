8dfb49a51d56d2ca85da0f9bb9a510eb4b520df9
Chris Lewis <me@chrislewis.au>
2026-06-23 18:04:24 -0400

    task-runner(decompile-unmatched): fix 'func_80043040'

--- src/437C0.c
+++ src/437C0.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+extern s32 D_801101A0[][5];
+
 #pragma GLOBAL_ASM("asm/nonmatchings/437C0/func_80042BC0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/437C0/func_80042C28.s")
@@ -14,7 +16,9 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/437C0/func_80042FA0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/437C0/func_80043040.s")
+s32 func_80043040(s32 arg0) {
+    return D_801101A0[arg0][0];
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/437C0/func_80043060.s")
 
