c1ea972ba9b6bc73bfcbb6b99d83db3f80f393d8
Chris Lewis <me@chrislewis.au>
2026-06-23 17:56:12 -0400

    task-runner(decompile-unmatched): fix 'func_8002C318'

--- src/2C160.c
+++ src/2C160.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+extern void func_80071824(void *, void *);
+extern void func_8002C18C(void);
+
 #pragma GLOBAL_ASM("asm/nonmatchings/2C160/func_8002B560.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/2C160/func_8002B8B4.s")
@@ -22,7 +25,12 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/2C160/func_8002C18C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/2C160/func_8002C318.s")
+void func_8002C318(void *arg0) {
+    *(s16 *)((s32)arg0 + 0x18) = -0x28;
+    *(s16 *)((s32)arg0 + 0x1A) = 0xC;
+    *(s16 *)((s32)arg0 + 0x1C) = 0xC;
+    func_80071824(arg0, func_8002C18C);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/2C160/func_8002C350.s")
 
