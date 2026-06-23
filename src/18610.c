cfe9a079cc7e9209836dfdbd4a38a862daccd65a
Chris Lewis <me@chrislewis.au>
2026-06-23 18:02:55 -0400

    task-runner(decompile-unmatched): fix 'func_800179D4'

--- src/18610.c
+++ src/18610.c
@@ -2,12 +2,19 @@
 
 extern void func_80071824(void *, void *);
 extern void func_80018C80(void);
+extern void func_800177F8(void);
 
 #pragma GLOBAL_ASM("asm/nonmatchings/18610/func_800171F0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/18610/func_800177F8.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/18610/func_800179D4.s")
+void func_800179D4(void *arg0) {
+    *(s16 *)((s32)arg0 + 0x18) = -0x70;
+    *(s16 *)((s32)arg0 + 0x1A) = -0x1C;
+    *(s16 *)((s32)arg0 + 0x1C) = 0;
+    *(s8 *)((s32)arg0 + 0x20) = 0;
+    func_80071824(arg0, func_800177F8);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/18610/func_80017A10.s")
 
