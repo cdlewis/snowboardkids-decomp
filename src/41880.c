fff68c550229bcd0791c6eadbc3b8b24c40c5663
Chris Lewis <me@chrislewis.au>
2026-06-23 18:58:00 -0400

    Match small state transition functions

--- src/41880.c
+++ src/41880.c
@@ -1,7 +1,23 @@
 #include "common.h"
 
-extern s32 func_80043040(s16);
+typedef struct Object41880 {
+    char pad0[0xC];
+    s32 unkC;
+    s32 unk10;
+    s16 unk14;
+    s16 unk16;
+    char pad18[0x11C];
+    s32 unk134;
+    s32 unk138;
+    s32 unk13C;
+    s16 unk140;
+    s16 unk142;
+    s16 unk144;
+} Object41880;
+
+extern Object41880 *func_80043040(s16);
 extern s16 D_8011218A[];
+extern void func_80041E90(Object41880 *arg0);
 extern void func_800437F0(void *, void *, s32);
 extern u8 D_5E34A0;
 extern u8 D_608560;
@@ -34,10 +50,34 @@ void func_80041E60(s32 arg0) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/41880/func_80041FB4.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/41880/func_80042034.s")
+void func_80042034(s32 arg0) {
+    Object41880 *temp_v0;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/41880/func_8004209C.s")
+    temp_v0 = func_80043040(D_8011218A[arg0]);
+    temp_v0->unk14--;
+    if (temp_v0->unk14 <= 0) {
+        temp_v0->unk14 = temp_v0->unk16;
+        temp_v0->unk10 = temp_v0->unkC;
+    }
+    func_80041E90(temp_v0);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/41880/func_800420FC.s")
+void func_8004209C(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    Object41880 *temp_v0;
+
+    temp_v0 = func_80043040(D_8011218A[arg0]);
+    temp_v0->unk134 = arg1;
+    temp_v0->unk138 = arg2;
+    temp_v0->unk13C = arg3;
+}
+
+void func_800420FC(s32 arg0, s16 arg1, s16 arg2, s16 arg3) {
+    Object41880 *temp_v0;
+
+    temp_v0 = func_80043040(D_8011218A[arg0]);
+    temp_v0->unk140 = arg1;
+    temp_v0->unk142 = arg2;
+    temp_v0->unk144 = arg3;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/41880/func_8004215C.s")
