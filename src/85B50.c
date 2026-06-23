49701db78192a0900e56c408b1097b12a41f6d2e
Chris Lewis <me@chrislewis.au>
2026-06-23 19:31:33 -0400

    Match camera collision helper

--- src/85B50.c
+++ src/85B50.c
@@ -134,7 +134,18 @@ s32 func_80085474(void *arg0) {
     return 0;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/85B50/func_800854E8.s")
+s32 func_800854E8(void *arg0) {
+    if (*(s32 *)((u8 *)arg0 + 0x2FC) & 0x375000) {
+        return 1;
+    }
+    if (*(s16 *)((u8 *)arg0 + 0x320) != 0) {
+        return 1;
+    }
+    if (D_80121D50 < 0x11) {
+        D_80121D50 = 0x11;
+    }
+    return 0;
+}
 
 s32 func_80085544(void *arg0) {
     if (*(s32 *)((u8 *)arg0 + 0x2FC) & 0x775000) {
