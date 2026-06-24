fc4177a7737278760bcb0a5cca0e50720b352c5a
Chris Lewis <me@chrislewis.au>
2026-06-24 17:14:28 -0400

    Match func_80069678

--- src/67360.c
+++ src/67360.c
@@ -8,12 +8,33 @@ extern void func_80097C18(void *, s16);
 extern void func_80098590(void *, void *, void *);
 extern void *func_800711D0(void *, s32, s32);
 extern void *func_80071408(void *, s32, s32);
+extern void func_800483FC(void *, void *, void *);
 extern char D_800D9BD8[];
+extern s32 D_801248D4;
 extern s16 D_80112168;
 extern u8 D_80121B56;
 extern void func_80067364(void *);
 extern void func_800674B4(void *);
-extern void func_80069678(void);
+
+typedef struct {
+    char pad0[0x18];
+    /* 0x18 */ s32 unk18;
+    /* 0x1C */ s32 unk1C;
+    /* 0x20 */ s32 unk20;
+    /* 0x24 */ s32 unk24;
+    /* 0x28 */ s32 unk28;
+    /* 0x2C */ s32 unk2C;
+    /* 0x30 */ s16 unk30;
+    /* 0x32 */ s16 unk32;
+    /* 0x34 */ s16 unk34;
+    /* 0x36 */ s16 unk36;
+    /* 0x38 */ s16 unk38;
+    /* 0x3A */ s16 unk3A;
+    /* 0x3C */ s16 unk3C;
+} Struct69678;
+
+extern void func_8006935C(Struct69678 *);
+extern void func_80069678(Struct69678 *);
 
 typedef struct Scratch674B4 {
     char scratch[0x28];
@@ -85,7 +106,29 @@ void func_8006752C(s32 arg0, s32 arg1, s32 arg2, s16 arg3, s16 arg4) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/67360/func_8006935C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/67360/func_80069678.s")
+void func_80069678(Struct69678 *arg0) {
+    s32 temp_v0;
+    Struct69678 *temp_a2 = arg0;
+
+    if (D_80121B56 == 0) {
+        arg0->unk3C--;
+    }
+    if (temp_a2->unk3C != 0) {
+        if (D_80121B56 == 0) {
+            temp_v0 = temp_a2->unk28;
+            temp_a2->unk18 += temp_a2->unk24;
+            temp_a2->unk1C += temp_v0;
+            temp_a2->unk20 += temp_a2->unk2C;
+            temp_a2->unk28 = temp_v0 + 0xFFFF0000;
+            temp_a2->unk30 += temp_a2->unk36;
+            temp_a2->unk32 += temp_a2->unk38;
+            temp_a2->unk34 += temp_a2->unk3A;
+        }
+        func_800483FC(&D_801248D4, func_8006935C, temp_a2);
+        return;
+    }
+    func_800716E4(temp_a2);
+}
 
 void func_80069754(void *arg0) {
     char sp28[0x20];
