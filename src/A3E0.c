7cfd6d43c149534907ba41956c56da8a4b2e21c0
Chris Lewis <me@chrislewis.au>
2026-06-24 10:44:23 -0400

    Match func_8000BBB4

--- src/A3E0.c
+++ src/A3E0.c
@@ -6,12 +6,30 @@ typedef struct ObjectA3E0 {
     char pad30[0x80];
 } ObjectA3E0;
 
+typedef struct {
+    char pad0[0x20];
+    s32 unk20;
+} Struct801235B8A3E0;
+
+typedef struct {
+    char pad0[4];
+    u8 unk4;
+    char pad5[3];
+    u8 unk8;
+    char pad9[0x20];
+    u8 unk29;
+} Struct8010AF18;
+
 extern ObjectA3E0 *D_800EC9C4;
 extern s16 D_8010ADF0;
+extern u8 D_8010AECC;
+extern Struct8010AF18 D_8010AF18;
 extern u8 D_8010AF41;
 extern s8 D_8010AF74;
 extern ObjectA3E0 D_801121E0[];
 extern u8 D_80121B55;
+extern Struct801235B8A3E0 *D_801235B8;
+extern void func_8000B220(void);
 extern void func_8000B7B8(void);
 extern void func_8007105C(void);
 extern void func_8009956C(void *, s32);
@@ -45,7 +63,24 @@ void func_8000BAFC(void) {
     func_8007105C();
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000BBB4.s")
+void func_8000BBB4(void) {
+    ObjectA3E0 *var_s1;
+    s32 var_s0;
+
+    if (D_801235B8->unk20 == 5) {
+        if (D_8010AECC == 0) {
+            D_8010AF18.unk4 = 1;
+        } else {
+            D_8010AF18.unk8 = 1;
+        }
+        D_8010AF18.unk29 = 0;
+        D_801235B8->unk20 = 0;
+        D_8010ADF0 = 0;
+        func_8009956C(func_8000B220, 0);
+    }
+ do { var_s0 = 0; if (D_80121B55 > 0) { var_s1 = D_801121E0; do { ; (D_800EC9C4 = var_s1)->unk2C(); var_s0 += 1; var_s1 += 1; } while (var_s0 < D_80121B55); } } while (0);
+    func_8007105C();
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000BCA0.s")
 
