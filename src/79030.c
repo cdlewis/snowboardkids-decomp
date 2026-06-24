25d1ebae31bfd6f74d96fec9ab20cd78bec85be5
Chris Lewis <me@chrislewis.au>
2026-06-23 23:02:22 -0400

    Match small 79030 text draw helpers

--- src/79030.c
+++ src/79030.c
@@ -2,17 +2,35 @@
 
 extern s32 func_80043040(s16);
 extern void func_80045A78(s32, s32, s32, s32);
+extern void func_80048278(s32, s32, char *, s32);
+extern int sprintf(char *, const char *, ...);
 extern s16 D_8011216E;
+extern s16 D_801222F6;
 
 #pragma GLOBAL_ASM("asm/nonmatchings/79030/func_80078430.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/79030/func_80078568.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/79030/func_80078974.s")
+const char D_800E1730[] = "Point";
+const char D_800E1738[] = "Time Limit";
+
+void func_80078974(s32 arg0) {
+    func_80048278(0x60, -0x61, (char *)D_800E1730, 5);
+    func_80048278(0x38, 0x47, (char *)D_800E1738, 5);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/79030/func_800789C0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/79030/func_80078D3C.s")
+const char D_800E1760[] = "Time Limit";
+const char D_800E176C[] = "/%d";
+
+void func_80078D3C(s32 arg0) {
+    char sp18[0x20];
+
+    func_80048278(0x38, 0x47, (char *)D_800E1760, 5);
+    sprintf(sp18, D_800E176C, D_801222F6);
+    func_80048278(0x70, -0x48, sp18, 6);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/79030/func_80078D9C.s")
 
