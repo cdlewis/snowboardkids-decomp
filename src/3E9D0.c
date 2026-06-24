54ae7236c2279a145011fca520e0b3c3135f9b20
Chris Lewis <me@chrislewis.au>
2026-06-24 10:52:39 -0400

    Match func_8003E514

--- src/3E9D0.c
+++ src/3E9D0.c
@@ -6,6 +6,20 @@ extern void func_8009954C(s32);
 extern s8 D_800DEED4;
 extern u8 D_80123750;
 extern u8 D_80123751;
+extern s16 D_800DEF14;
+extern void func_8005393C(void);
+extern void func_8006D780(s32);
+extern void func_8007105C(void);
+extern void func_80071664(void *, s32, s32, s32);
+extern void func_8009956C(void *, s32);
+void func_8003E5A8(void);
+
+typedef struct {
+    char pad[0x18];
+    s32 unk18;
+} Struct801235B8;
+
+extern Struct801235B8 *D_801235B8;
 
 #pragma GLOBAL_ASM("asm/nonmatchings/3E9D0/func_8003DDD0.s")
 
@@ -31,7 +45,18 @@ void func_8003DFB0(void) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/3E9D0/func_8003E45C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/3E9D0/func_8003E514.s")
+void func_8003E514(void) {
+    D_800DEF14 += 4;
+    if (D_800DEF14 >= 0xFF) {
+        D_801235B8->unk18 = 0xFF;
+        D_80123751 = 1;
+        func_8009956C(func_8003E5A8, 0);
+    }
+    func_80071664(func_8005393C, 5, 0x64, 0);
+    func_8006D780(0);
+    func_8006D780(1);
+    func_8007105C();
+}
 
 void func_8003E5A8(void) {
     if (D_80123750 == 2) {
