6f20381d9cc6e5acfbdae48c125ab8dac1d3cb47
Chris Lewis <me@chrislewis.au>
2026-06-24 17:05:10 -0400

    Match func_8002784C

--- src/24480.c
+++ src/24480.c
@@ -29,9 +29,27 @@
 #pragma GLOBAL_ASM("asm/nonmatchings/24480/func_800260E8.s")
 
 extern void func_80071824(void *, void *);
+extern void func_800483FC(void *, void *, void *);
 extern void func_800260E8(void);
 extern void func_80028FF0(void);
 extern u8 D_80121B55;
+extern s32 D_80124868;
+
+typedef struct Obj2784C {
+    char pad0[0x18];
+    s16 unk18;
+    char pad1A[0x42];
+    u8 unk5C;
+} Obj2784C;
+
+typedef struct Struct801235B8_24480 {
+    char pad0[0x20];
+    s32 unk20;
+} Struct801235B8_24480;
+
+extern Struct801235B8_24480 *D_801235B8;
+extern void func_800275E0(void);
+extern void func_80027AF8(void);
 
 void func_800263A4(void *arg0) {
     *(s16 *)((u8 *)arg0 + 0x18) = -0x108;
@@ -77,7 +95,31 @@ void func_800275A4(void *arg0) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/24480/func_800275E0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/24480/func_8002784C.s")
+void func_8002784C(Obj2784C *arg0) {
+    s32 var_v1;
+    Obj2784C *var_v0;
+    Obj2784C *temp_a2 = arg0;
+    Obj2784C *temp_a1;
+
+    temp_a1 = arg0;
+    if (arg0->unk18 < -0x74) {
+        var_v1 = 0;
+        if ((s32)arg0->unk5C > 0) {
+            var_v0 = arg0;
+            do {
+                var_v0->unk18 += 0x20;
+                if (var_v0->unk18 >= -0x74) {
+                    var_v0->unk18 = -0x74;
+                }
+                var_v1++;
+                var_v0 = (Obj2784C *)((u8 *)var_v0 + 2);
+            } while (var_v1 < (s32)temp_a1->unk5C);
+        }
+    } else if (D_801235B8->unk20 == 4) {
+        func_80071824(temp_a2, func_80027AF8);
+    }
+    func_800483FC(&D_80124868, func_800275E0, temp_a2);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/24480/func_80027914.s")
 
