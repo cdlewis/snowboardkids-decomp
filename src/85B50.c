0c00c4e35f80adfbacdd5dba955a906ca9d0af87
Chris Lewis <me@chrislewis.au>
2026-06-24 16:15:00 -0400

    Match 85B50 state checks

--- src/85B50.c
+++ src/85B50.c
@@ -1,10 +1,73 @@
 #include "common.h"
 
+typedef struct {
+    char pad0[0x2EC];
+    s16 unk2EC;
+    char pad2EE[0xE];
+    s32 unk2FC;
+} Actor85B50;
+
 extern s32 D_80121D50;
+extern s32 D_80121D54;
+extern s32 D_80121D58;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/85B50/func_80084F50.s")
+s32 func_80084F50(Actor85B50 *arg0) {
+    s32 temp_v0;
+    s32 var_v1;
+    s32 temp_t0;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/85B50/func_80085010.s")
+    temp_v0 = arg0->unk2FC;
+    if (temp_v0 & 0x379000) {
+        return 1;
+    }
+    if (!(temp_v0 & 0x400000)) {
+        var_v1 = D_80121D54 - arg0->unk2EC;
+        var_v1 += 0x800;
+        temp_t0 = var_v1 & 0xFFF;
+        var_v1 = (s16) temp_t0;
+        if (temp_v0 & 0x400) {
+            var_v1 = (s16) (var_v1 + 0x800);
+        }
+        var_v1 += 0x200;
+        var_v1 = (s16) (var_v1 & 0xFFF);
+        if ((D_80121D58 < 0x30000) || (var_v1 >= 0x401)) {
+            return 1;
+        }
+    }
+    if (D_80121D50 < 5) {
+        D_80121D50 = 5;
+    }
+    return 0;
+}
+
+s32 func_80085010(Actor85B50 *arg0) {
+    s32 temp_v0;
+    s32 var_v1;
+    s32 temp_t0;
+
+    temp_v0 = arg0->unk2FC;
+    if (temp_v0 & 0x379000) {
+        return 1;
+    }
+    if (!(temp_v0 & 0x400000)) {
+        var_v1 = D_80121D54 - arg0->unk2EC;
+        var_v1 += 0x800;
+        temp_t0 = var_v1 & 0xFFF;
+        var_v1 = (s16) temp_t0;
+        if (temp_v0 & 0x400) {
+            var_v1 = (s16) (var_v1 + 0x800);
+        }
+        var_v1 += 0x200;
+        var_v1 = (s16) (var_v1 & 0xFFF);
+        if ((D_80121D58 < 0x30000) || (var_v1 >= 0x401)) {
+            return 1;
+        }
+    }
+    if (D_80121D50 < 6) {
+        D_80121D50 = 6;
+    }
+    return 0;
+}
 
 s32 func_800850D0(void *arg0) {
     if (*(s32 *)((u8 *)arg0 + 0x2FC) & 0x37D000) {
