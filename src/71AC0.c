dc75e1a5a4c4c92b3e4e1420610081e18e19c025
Chris Lewis <me@chrislewis.au>
2026-06-28 23:32:44 -0400

    Match four functions

--- src/71AC0.c
+++ src/71AC0.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+typedef struct {
+    u8 pad0[0x10];
+    s16 unk10;
+} Struct71AC0;
+
+void *func_800711D0(void *, s32, s32);
+
 #pragma GLOBAL_ASM("asm/nonmatchings/71AC0/func_80070EC0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/71AC0/func_8007105C.s")
@@ -17,7 +24,7 @@ void func_800711C8(void) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/71AC0/func_80071664.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/71AC0/func_800716A4.s")
+void *func_800716A4(void *arg0, s32 arg1, s32 arg2, s32 arg3){ void *t=func_800711D0(arg0,arg1&0xFFFF,arg2); if(t!=NULL){((Struct71AC0 *)t)->unk10=arg3;} return t;}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/71AC0/func_800716E4.s")
 
