0cda5e92d9b4bb7664ca79ceb02f734f5b0530da
Chris Lewis <me@chrislewis.au>
2026-07-03 15:37:17 -0400

    Rename game setup menu segment

---
+++ src/game_setup_menu.c
@@ -0,0 +1,16 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_setup_menu/func_80003140.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_setup_menu/func_8000337C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_setup_menu/func_800035F8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_setup_menu/func_80003798.s")
+
+void __dummy(void) {
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_setup_menu/func_80004164.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/game_setup_menu/func_800045D8.s")
