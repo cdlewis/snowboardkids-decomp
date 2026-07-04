f83f1b7be0a6b6784239478992270bdea896867c
Chris Lewis <me@chrislewis.au>
2026-07-04 04:38:05 -0400

    Clean up course select menu

--- src/course_select_menu.c
+++ src/course_select_menu.c
@@ -1,24 +1,27 @@
 #include "common.h"
 
+typedef struct {
+    /* 0x00 */ u8 pad0[0x18];
+    /* 0x18 */ s32 fade;
+    /* 0x1C */ s32 timer;
+    /* 0x20 */ s32 screenState;
+} CourseSelectMenuState;
+
+typedef struct {
+    /* 0x00 */ u8 pad0[4];
+    /* 0x04 */ u8 playerOneCourseDecided;
+    /* 0x05 */ u8 pad5[3];
+    /* 0x08 */ u8 playerTwoCourseDecided;
+    /* 0x09 */ u8 pad9[0x1F];
+    /* 0x28 */ u8 unk28;
+    /* 0x29 */ u8 transitionState;
+} CourseSelectStatus;
+
 typedef struct ObjectA3E0 {
-    char pad0[0x2C];
-    void (*unk2C)(void);
-    char pad30[0x80];
+    /* 0x00 */ u8 pad0[0x2C];
+    /* 0x2C */ void (*unk2C)(void);
+    /* 0x30 */ u8 pad30[0x80];
 } ObjectA3E0;
-
-typedef struct {
-    char pad0[0x20];
-    s32 unk20;
-} Struct801235B8A3E0;
-
-typedef struct {
-    char pad0[4];
-    u8 unk4;
-    char pad5[3];
-    u8 unk8;
-    char pad9[0x20];
-    u8 unk29;
-} Struct8010AF18;
 
 extern ObjectA3E0 *D_800EC9C4;
 extern f32 D_800E0A38;
@@ -27,8 +30,7 @@ extern s16 D_8010AED0;
 extern s16 D_8010ADF0;
 extern s8 D_8010ADF8;
 extern u8 D_8010AECC;
-extern Struct8010AF18 D_8010AF18;
-extern u8 D_8010AF41;
+extern CourseSelectStatus D_8010AF18;
 extern s8 D_8010AF72;
 extern s8 D_8010AF73;
 extern s16 D_8010AF76;
@@ -37,7 +39,7 @@ extern ObjectA3E0 D_801121E0[];
 extern ObjectA3E0 D_80112340;
 extern u8 D_80121B55;
 extern u8 D_80121D86;
-extern Struct801235B8A3E0 *D_801235B8;
+extern CourseSelectMenuState *D_801235B8;
 extern void func_8000B220(void);
 extern void func_8000B7B8(void);
 extern void func_8002F854(void);
@@ -47,24 +49,24 @@ extern void func_8007105C(void);
 extern void func_80071408(void *, s32, s32);
 extern void func_8009956C(void *, s32);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_800097E0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_800097E0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_80009C48.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_80009C48.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000A048.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000A048.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000A214.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000A214.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000AFE8.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000AFE8.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000B220.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000B220.s")
 
 void func_8000B690(void) {
     ObjectA3E0 *var_s1;
     s32 var_s0;
     s8 temp_v0;
 
-    if (D_801235B8->unk20 == 2) {
+    if (D_801235B8->screenState == 2) {
         func_80071408(func_8002F854, 0, 0x63);
         temp_v0 = D_800ECA2F[D_80121D86];
         D_8010AF72 = temp_v0 % 7;
@@ -73,21 +75,23 @@ void func_8000B690(void) {
         D_8010ADF8 = 0;
         func_8009956C(func_8000B7B8, 0);
     }
+
  do { var_s0 = 0; if (D_80121B55 > 0) { var_s1 = D_801121E0; do { ; (D_800EC9C4 = var_s1)->unk2C(); var_s0 += 1; var_s1 += 1; } while (var_s0 < D_80121B55); } } while (0);
     func_8007105C();
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000B7B8.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000B7B8.s")
 
 void func_8000BAFC(void) {
     ObjectA3E0 *var_s1;
     s32 var_s0;
 
-    if (D_8010AF41 == 2) {
+    if (D_8010AF18.transitionState == 2) {
         D_8010ADF0 = 0;
         D_8010AF74 = 0;
         func_8009956C(func_8000B7B8, 0);
     }
+
  do { var_s0 = 0; if (D_80121B55 > 0) { var_s1 = D_801121E0; do { ; (D_800EC9C4 = var_s1)->unk2C(); var_s0 += 1; var_s1 += 1; } while (var_s0 < D_80121B55); } } while (0);
     func_8007105C();
 }
@@ -96,24 +100,25 @@ void func_8000BBB4(void) {
     ObjectA3E0 *var_s1;
     s32 var_s0;
 
-    if (D_801235B8->unk20 == 5) {
+    if (D_801235B8->screenState == 5) {
         if (D_8010AECC == 0) {
-            D_8010AF18.unk4 = 1;
+            D_8010AF18.playerOneCourseDecided = 1;
         } else {
-            D_8010AF18.unk8 = 1;
+            D_8010AF18.playerTwoCourseDecided = 1;
         }
-        D_8010AF18.unk29 = 0;
-        D_801235B8->unk20 = 0;
+        D_8010AF18.transitionState = 0;
+        D_801235B8->screenState = 0;
         D_8010ADF0 = 0;
         func_8009956C(func_8000B220, 0);
     }
+
  do { var_s0 = 0; if (D_80121B55 > 0) { var_s1 = D_801121E0; do { ; (D_800EC9C4 = var_s1)->unk2C(); var_s0 += 1; var_s1 += 1; } while (var_s0 < D_80121B55); } } while (0);
     func_8007105C();
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000BCA0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000BCA0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000BEC0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000BEC0.s")
 
 void func_8000C010(void) {
     ObjectA3E0 *var_s0;
@@ -126,8 +131,8 @@ void func_8000C010(void) {
     if (D_8010AF76 == 0) {
         func_80070614(1);
         func_8009956C(func_8000B7B8, 0);
-        D_8010AF41 = 2;
+        D_8010AF18.transitionState = 2;
  D_8010AED0 = 0; } var_s0 = D_801121E0; do { (D_800EC9C4 = var_s0)->unk2C(); var_s0 += 1; } while (var_s0 != (&D_80112340)); func_8007105C();
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/A3E0/func_8000C114.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000C114.s")
