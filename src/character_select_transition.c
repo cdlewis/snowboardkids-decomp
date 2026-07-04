13e6bcc9bf1272dfaf2bd89f3e5f52899fc1539e
Chris Lewis <me@chrislewis.au>
2026-07-04 01:47:29 -0400

    Clean up character select transition segment

--- src/character_select_transition.c
+++ src/character_select_transition.c
@@ -1,10 +1,5 @@
 #include "common.h"
-
-typedef struct {
-    char pad[0x18];
-    s32 unk18;
-    s32 unk1C;
-} Struct801235B8;
+#include "character_select_flow.h"
 
 extern s32 func_80013F88(s32, s32, s32);
 extern void func_8007105C(void);
@@ -12,19 +7,19 @@ extern void func_80045914(void);
 extern void func_80099658(s32);
 extern void func_8009954C(s32);
 
-extern Struct801235B8 *D_801235B8;
+extern CharacterSelectFlowState *D_801235B8;
 extern s8 D_800DEED4;
 extern u8 D_80123750;
 extern u8 D_80123751;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/7B30/func_80006F30.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/character_select_transition/func_80006F30.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/7B30/func_80007840.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/character_select_transition/func_80007840.s")
 
 void func_8000854C(void) {
-    if (D_801235B8->unk18 != 0xFF) {
-        D_801235B8->unk18 = func_80013F88((s16) D_801235B8->unk18, 0x24, 1);
-        if (D_801235B8->unk18 == 0xFF) {
+    if (D_801235B8->fade != 0xFF) {
+        D_801235B8->fade = func_80013F88((s16) D_801235B8->fade, 0x24, 1);
+        if (D_801235B8->fade == 0xFF) {
             D_80123751 = 1;
         } else {
             func_8007105C();
