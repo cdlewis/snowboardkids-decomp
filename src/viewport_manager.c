0b9a28bceb3640d3147b1afa9474645118a5e543
Chris Lewis <me@chrislewis.au>
2026-07-04 01:20:31 -0400

    Clean up viewport manager structs

---
+++ src/viewport_manager.c
@@ -0,0 +1,41 @@
+#include "viewport_manager.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/viewport_manager/func_800704F0.s")
+
+void func_80070614(s32 arg0) {
+    ViewportState *viewport = &D_801124B0[arg0];
+
+    viewport->active = 0;
+    viewport->screenBoundsValid = 0;
+    viewport->unk2 = 0;
+    viewport->overlayActive = 0;
+    viewport->overlayR = 0;
+    viewport->overlayG = 0;
+    viewport->overlayB = 0;
+    viewport->overlayAlpha = 0;
+    viewport->unk14 = 0x1FF;
+    viewport->unk16 = 0;
+    viewport->unk1C = 0x1FF;
+    viewport->unk1E = 0;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/viewport_manager/func_8007066C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/viewport_manager/func_80070860.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/viewport_manager/func_80070A70.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/viewport_manager/func_80070C64.s")
+
+void func_80070E58(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    ViewportState *viewport = &D_801124B0[arg0];
+
+    viewport->overlayActive = 1;
+    viewport->overlayR = arg1;
+    viewport->overlayG = arg2;
+    viewport->overlayB = arg3;
+}
+
+void func_80070E90(s32 arg0) {
+    D_801124B0[arg0].unk2 = 1;
+}
