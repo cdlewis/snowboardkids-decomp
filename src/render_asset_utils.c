f9c5a3231d9388c1ca44166c4942cc42b8d094a0
Chris Lewis <me@chrislewis.au>
2026-07-03 23:58:38 -0400

    Clean up render asset utilities

---
+++ src/render_asset_utils.c
@@ -0,0 +1,310 @@
+#include "common.h"
+
+typedef struct {
+    /* 0x0 */ s32 unk0;
+    /* 0x4 */ s32 entryCount;
+    /* 0x8 */ char entriesStart[1];
+} AssetTable;
+
+typedef struct {
+    /* 0x0 */ s32 imageOffset;
+    /* 0x4 */ u16 textureIndex;
+    /* 0x6 */ u8 width;
+    /* 0x7 */ u8 height;
+} AssetTableEntry;
+
+typedef struct RenderCallbackNode {
+    struct RenderCallbackNode *next;
+    void (*callback)(s32);
+    s32 arg;
+} RenderCallbackNode;
+
+typedef struct {
+    s32 words[16];
+} GfxCommandBlock;
+
+typedef struct {
+    s32 unk0;
+    s32 unk4;
+    s32 unk8;
+} GfxCommandTriple;
+
+typedef struct {
+    /* 0x00 */ s16 unk0;
+    /* 0x02 */ s16 unk2;
+    /* 0x04 */ s16 unk4;
+    /* 0x06 */ s16 unk6;
+    /* 0x08 */ s16 unk8;
+    /* 0x0A */ s16 unkA;
+    /* 0x0C */ s16 unkC;
+    /* 0x0E */ s16 unkE;
+    /* 0x10 */ s16 unk10;
+    /* 0x12 */ char pad12[2];
+    /* 0x14 */ s32 unk14;
+    /* 0x18 */ s32 unk18;
+    /* 0x1C */ s32 unk1C;
+} GfxCommandSource;
+
+typedef struct {
+    /* 0x00 */ s32 unk0;
+    /* 0x04 */ s32 unk4;
+    /* 0x08 */ s32 unk8;
+    /* 0x0C */ s32 unkC;
+    /* 0x10 */ s32 unk10;
+    /* 0x14 */ s32 unk14;
+    /* 0x18 */ s32 unk18;
+    /* 0x1C */ s32 unk1C;
+    /* 0x20 */ s32 unk20;
+    /* 0x24 */ s32 unk24;
+    /* 0x28 */ s32 unk28;
+    /* 0x2C */ s32 unk2C;
+    /* 0x30 */ s32 unk30;
+    /* 0x34 */ s32 unk34;
+    /* 0x38 */ s32 unk38;
+    /* 0x3C */ s32 unk3C;
+} GfxCommandDest;
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_800458E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80045914.s")
+
+void *func_8004597C(void *arg0, u32 arg1) {
+    return (void *)((u8 *)arg0 + (arg1 & 0xFFFFFF));
+}
+
+void func_80045990(u8 *arg0, u16 arg1, void **arg2, void **arg3) {
+    AssetTableEntry *temp_v1;
+    u8 *temp_v0;
+    short idx;
+
+    temp_v1 = (AssetTableEntry *)((s32)arg0 + (arg1 * sizeof(AssetTableEntry)));
+    temp_v0 = arg0 + (((AssetTable *)arg0)->entryCount * sizeof(AssetTableEntry));
+    idx = 1;
+    *arg2 = (void *)(arg0 + temp_v1[idx].imageOffset);
+    temp_v0 += 8;
+    *arg3 = (void *)((temp_v1[idx].textureIndex << 5) + temp_v0);
+}
+
+void func_800459D4(u8 *arg0, u16 arg1, u16 arg2, void **arg3, void **arg4) {
+    u8 *temp_v0;
+    AssetTableEntry *temp_v1;
+
+    temp_v1 = (AssetTableEntry *)((s32)arg0 + (arg1 * sizeof(AssetTableEntry)));
+    temp_v0 = arg0 + (((AssetTable *)arg0)->entryCount * sizeof(AssetTableEntry));
+    *arg3 = (void *)(arg0 + temp_v1[1].imageOffset);
+    temp_v0 += 8;
+    *arg4 = (void *)((arg2 << 5) + temp_v0);
+}
+
+void func_80045A1C(u8 *arg0, u16 arg1, void **arg2, void **arg3, s16 *arg4, s16 *arg5) {
+    AssetTableEntry *temp_v1;
+    u8 *temp_v0;
+    short idx;
+
+    temp_v1 = (AssetTableEntry *)((s32)arg0 + (arg1 * sizeof(AssetTableEntry)));
+    temp_v0 = arg0 + (((AssetTable *)arg0)->entryCount * sizeof(AssetTableEntry));
+    idx = 1;
+    *arg2 = (void *)(arg0 + temp_v1[idx].imageOffset);
+    temp_v0 += 8;
+    *arg3 = (void *)((temp_v1[idx].textureIndex << 5) + temp_v0);
+    *arg4 = temp_v1[idx].width;
+    *arg5 = temp_v1[idx].height;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80045A78.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80045E84.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80046358.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80046748.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80046970.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80046D68.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80047174.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_8004767C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80047B84.s")
+
+extern s32 func_80043040(s16);
+extern s16 D_8011213C;
+extern s16 D_801121B0;
+extern s16 D_801121B2;
+extern void *D_801121B4;
+
+void func_80047E38(void) {
+    s32 v0 = func_80043040(D_8011213C);
+    AssetTable *assetTable = (AssetTable *)v0;
+
+    D_801121B4 = (void *)((assetTable->entryCount * sizeof(AssetTableEntry)) + (u8 *)assetTable + sizeof(AssetTableEntry));
+    D_801121B2 = -1;
+    D_801121B0 = -1;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80047E88.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80048278.s")
+
+extern s16 D_80112130;
+s32 func_80043040(s16);                             /* extern */
+extern s32 D_801121B8;
+s16 func_80042D58(s32);
+
+void func_80048338(void) {
+    D_80112130 = func_80042D58(0x4000);
+}
+
+void func_8004835C(void) {
+    D_801121B8 = func_80043040(D_80112130);
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80048388.s")
+
+extern void *func_80048388(s32);
+
+void func_800483FC(RenderCallbackNode **arg0, void (*arg1)(s32), s32 arg2) {
+    RenderCallbackNode *temp_v0 = func_80048388(sizeof(RenderCallbackNode));
+
+    if (temp_v0 != NULL) {
+        temp_v0->next = *arg0;
+        temp_v0->callback = arg1;
+        temp_v0->arg = arg2;
+        *arg0 = temp_v0;
+    }
+}
+
+extern s32 D_80124830;
+extern s32 D_80124904;
+
+void func_8004845C(RenderCallbackNode **arg0) {
+    RenderCallbackNode *s0 = *arg0;
+    if (s0 != NULL) {
+loop:
+        if ((u32)((D_80124830 - D_80124904) - 0x5B8) < 0x14181U) {
+            s0->callback(s0->arg);
+            s0 = s0->next;
+            if (s0 != NULL) {
+                goto loop;
+            }
+        }
+    }
+}
+
+extern s16 D_80112132;
+extern s16 D_80112134;
+
+void func_800484F0(void) {
+    D_80112132 = func_80042D58(0x8000);
+    D_80112134 = func_80042D58(0x8000);
+}
+
+extern u32 D_80123754;
+extern u8 *D_801121C0;
+extern u8 *D_801121BC;
+
+void func_80048524(s32 arg0) {
+    D_80123754 = 0;
+    if (arg0 == 0) {
+        D_801121BC = D_801121C0 = func_80043040(D_80112132);
+    } else {
+        D_801121BC = D_801121C0 = func_80043040(D_80112134);
+    }
+}
+
+void *func_80048594(s32 size) {
+    u8 *oldPtr = D_801121BC;
+    u8 *newPtr = ((0, oldPtr)) + ((((u32)(size + 3)) >> 2) * 4);
+    s32 new_var2;
+
+    new_var2 = newPtr - D_801121C0;
+    newPtr++;
+    newPtr--;
+
+    if (D_801121BC) {
+    }
+
+    if ((u32)(newPtr - D_801121C0) >= 0x8000) {
+        return 0;
+    }
+
+    D_801121BC = newPtr;
+    D_80123754 = new_var2;
+
+    return oldPtr;
+}
+
+extern void osWritebackDCache(void *, s32);
+
+void func_800485E8(s32 arg0) {
+    if (arg0 == 0) {
+        osWritebackDCache((void *)func_80043040(D_80112132), D_80123754);
+    } else {
+        osWritebackDCache((void *)func_80043040(D_80112134), D_80123754);
+    }
+}
+
+extern void *func_80048594(s32);
+
+void *func_8004864C(GfxCommandBlock *arg0) {
+    GfxCommandBlock *p = func_80048594(sizeof(GfxCommandBlock));
+    if (p == NULL) {
+        return NULL;
+    }
+    *p = *arg0;
+    return p;
+}
+
+void func_800486BC(void *arg0, void *arg1) {
+    GfxCommandSource *src = arg0;
+    GfxCommandDest *dst = arg1;
+
+    dst->unk0 = ((src->unk2 >> 12) & 0xFFFF) | ((src->unk0 << 4) & 0xFFFF0000);
+    dst->unk4 = (src->unk4 << 4) & 0xFFFF0000;
+    dst->unk8 = ((src->unk8 >> 12) & 0xFFFF) | ((src->unk6 << 4) & 0xFFFF0000);
+    dst->unkC = (src->unkA << 4) & 0xFFFF0000;
+    dst->unk10 = ((src->unkE >> 12) & 0xFFFF) | ((src->unkC << 4) & 0xFFFF0000);
+    dst->unk14 = (src->unk10 << 4) & 0xFFFF0000;
+    dst->unk18 = ((src->unk18 >> 16) & 0xFFFF) | (src->unk14 & 0xFFFF0000);
+    dst->unk1C = (src->unk1C & 0xFFFF0000) | 1;
+    dst->unk20 = ((src->unk2 << 4) & 0xFFFF) | ((src->unk0 << 20) & 0xFFFF0000);
+    dst->unk24 = (src->unk4 << 20) & 0xFFFF0000;
+    dst->unk28 = ((src->unk8 << 4) & 0xFFFF) | ((src->unk6 << 20) & 0xFFFF0000);
+    dst->unk2C = (src->unkA << 20) & 0xFFFF0000;
+    dst->unk30 = ((src->unkE << 4) & 0xFFFF) | ((src->unkC << 20) & 0xFFFF0000);
+    dst->unk34 = (src->unk10 << 20) & 0xFFFF0000;
+    dst->unk38 = (src->unk18 & 0xFFFF) | ((src->unk14 << 16) & 0xFFFF0000);
+    dst->unk3C = (src->unk1C << 16) & 0xFFFF0000;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_8004885C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80048A38.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80048BBC.s")
+
+void func_80048C90(GfxCommandDest *arg0, GfxCommandTriple *arg1) {
+    arg0->unk18 = (s32) ((arg1->unk0 & 0xFFFF0000) | (((s32) arg1->unk4 >> 0x10) & 0xFFFF));
+    arg0->unk1C = (s32) ((arg1->unk8 & 0xFFFF0000) | 1);
+    arg0->unk38 = (s32) (((arg1->unk0 << 0x10) & 0xFFFF0000) | (arg1->unk4 & 0xFFFF));
+    arg0->unk3C = (s32) ((arg1->unk8 << 0x10) & 0xFFFF0000);
+}
+
+void func_80048CF8(GfxCommandBlock *arg0, GfxCommandBlock *arg1) {
+    arg1->words[6] = (arg0->words[5] & 0xFFFF0000) | ((arg0->words[6] >> 0x10) & 0xFFFF);
+    arg1->words[7] = (arg0->words[7] & 0xFFFF0000) | 1;
+    arg1->words[14] = ((arg0->words[5] << 0x10) & 0xFFFF0000) | (arg0->words[6] & 0xFFFF);
+    arg1->words[15] = (arg0->words[7] << 0x10) & 0xFFFF0000;
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80048D60.s")
+
+void func_80048E3C(void *arg0, void *arg1, void *arg2) {
+}
+
+void func_80048E4C(void *arg0, void *arg1, void *arg2, void *arg3) {
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/render_asset_utils/func_80048E60.s")
