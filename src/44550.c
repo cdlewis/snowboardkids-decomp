ad572b44f62e2f060f12294914956e870dbcf139
Chris Lewis <me@chrislewis.au>
2026-07-03 23:54:12 -0400

    Clean up course asset loader segment

--- src/44550.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/44550/func_80043950.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/44550/func_800440F4.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/44550/func_80044294.s")
