1963aa925b289ebd7f2c6e6494a3b1d4153ee65c
Chris Lewis <me@chrislewis.au>
2026-07-04 12:05:48 -0400

    Match func_800437F0

--- src/asset_decompression.c
+++ src/asset_decompression.c
@@ -16,6 +16,11 @@ typedef struct {
 } CompressedAssetHeader;
 
 typedef struct {
+    /* 0x00 */ s16 assetHandles[0x20];
+    /* 0x40 */ s16 compressedAssetHandle;
+} AssetHandleTable;
+
+typedef struct {
     u8 pad0[0x518];
     /* 0x518 */ u8 randomIndex;
 } RandomStateObject;
@@ -29,12 +34,13 @@ extern s16 D_800D4018;
 extern s16 D_801235B0;
 extern CompressedAssetHeader D_80110920;
 extern HuffmanNode D_80110928[];
+extern AssetHandleTable D_80112130;
 extern s32 D_80112128;
-extern s16 D_80112130[];
 extern s16 func_80042D58(s32);
 extern s16 func_80042EE4(s16);
 extern s32 func_80043040(s16);
-extern void func_80099C44(s32, s32, s32);
+extern void func_800433D8(u8, s32, s32, s32);
+extern void func_80099C44(u32, void *, s32);
 
 u8 func_800430D0(void) {
     D_800D4000++;
@@ -96,13 +102,26 @@ void func_8004331C(s16 arg0) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/asset_decompression/func_800433D8.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/asset_decompression/func_800437F0.s")
+void func_800437F0(s32 arg0, s32 arg1, s32 arg2) {
+    s16 *sp28;
+    s32 sp30;
+
+    func_80099C44(arg0, &D_80110920, 8);
+    D_80112130.assetHandles[arg2] = func_80042D58(D_80110920.compressedSize);
+    D_80112130.compressedAssetHandle = func_80042D58(arg1 - arg0);
+    func_80099C44(arg0, (void *)func_80043040(D_80112130.compressedAssetHandle), arg1 - arg0);
+    sp30 = func_80043040(D_80112130.compressedAssetHandle) + 5;
+    sp28 = &D_80112130.assetHandles[arg2];
+    func_800433D8(D_80110920.flags, sp30, func_80043040(*sp28), D_80110920.compressedSize);
+    func_80043040(*sp28);
+    D_80112130.compressedAssetHandle = func_80042EE4(D_80112130.compressedAssetHandle);
+}
 
 void func_800438EC(s32 arg0, s32 arg1, s32 arg2) {
     s32 temp_a0 = arg1 - arg0;
     s16 *temp_v1;
 
-    temp_v1 = &D_80112130[arg2];
+    temp_v1 = &D_80112130.assetHandles[arg2];
     *temp_v1 = func_80042D58(temp_a0);
-    func_80099C44(arg0, func_80043040(*temp_v1), temp_a0);
+    func_80099C44(arg0, (void *)func_80043040(*temp_v1), temp_a0);
 }
