627ce690f3edc764300957c3f2e63336a96d097c
Chris Lewis <me@chrislewis.au>
2026-07-04 23:22:17 -0400

    Match func_8000BCA0
    Co-authored-by: queueRAM <queueram@proton.me>

--- src/course_select_menu.c
+++ src/course_select_menu.c
@@ -8,12 +8,23 @@ typedef struct {
 } CourseSelectMenuState;
 
 typedef struct {
-    /* 0x00 */ u8 unk0[4];
-    /* 0x04 */ u8 playerOneCourseDecided;
-    /* 0x05 */ u8 unk5[3];
-    /* 0x08 */ u8 playerTwoCourseDecided;
-    /* 0x09 */ u8 unk9[7];
-    /* 0x10 */ u8 unk10[4];
+    union {
+        struct {
+            /* 0x00 */ u8 unk0[4];
+            /* 0x04 */ u8 playerOneCourseDecided;
+            /* 0x05 */ u8 unk5[3];
+            /* 0x08 */ u8 playerTwoCourseDecided;
+            /* 0x09 */ u8 unk9[7];
+            /* 0x10 */ u8 unk10[4];
+        };
+        struct {
+            /* 0x00 */ u8 unk0Array[4];
+            /* 0x04 */ u8 unk4Array[4];
+            /* 0x08 */ u8 unk8Array[4];
+            /* 0x0C */ u8 unkCArray[4];
+            /* 0x10 */ u8 unk10Array[4];
+        };
+    };
     /* 0x14 */ s16 unk14[4];
     /* 0x1C */ s16 unk1C[4];
     /* 0x24 */ u8 unk24[4];
@@ -25,6 +36,13 @@ typedef struct {
     /* 0x2F */ u8 unk2F[9];
 } CourseSelectStatus;
 
+typedef struct {
+    /* 0x00 */ u8 pad0[6];
+    /* 0x06 */ u8 unk6;
+    /* 0x07 */ u8 unk7;
+    /* 0x08 */ u8 unk8;
+} CourseSelectSelection;
+
 typedef struct ObjectA3E0 {
     /* 0x00 */ u8 pad0[0x2C];
     /* 0x2C */ void (*unk2C)(void);
@@ -34,6 +52,7 @@ typedef struct ObjectA3E0 {
 extern ObjectA3E0 *D_800EC9C4;
 extern f32 D_800E0A34;
 extern u16 D_800B34E0[];
+extern s16 D_800EC9D0;
 extern f32 D_800E0A38;
 extern s8 D_800DEED4;
 extern u8 D_800EC9F0[];
@@ -53,10 +72,12 @@ extern ObjectA3E0 D_801121E0[];
 extern ObjectA3E0 D_80112340;
 extern u8 D_80121B55;
 extern u8 D_80121D86;
+extern CourseSelectSelection D_80121D80;
 extern CourseSelectMenuState *D_801235B8;
 extern s32 D_801235B4;
 extern u8 D_80123750;
 extern u8 D_80123751;
+extern void func_80009C48(void);
 extern void func_8000B220(void);
 extern void func_8000B7B8(void);
 extern void func_8000C010(void);
@@ -141,7 +162,43 @@ void func_8000BBB4(void) {
     func_8007105C();
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/course_select_menu/func_8000BCA0.s")
+void func_8000BCA0(void) {
+    ObjectA3E0 *var_s1;
+    s32 var_s0;
+    s32 i;
+
+    if (D_801235B8->screenState == 0xB) {
+        func_8009956C(func_80009C48, 0);
+        D_801235B8->fade = 1;
+        D_801235B8->timer = 0;
+        D_801235B8->screenState = 0;
+        D_80121D80.unk8 = 0;
+        D_80121D80.unk6 = D_80121D80.unk6 % 3;
+        D_800EC9D0 = 0;
+        D_8010AECC = 0;
+        D_8010ADF0 = 0;
+
+        for (i = 0; i < 4; i++) {
+            D_8010AF18.unk0Array[i] = 0;
+            D_8010AF18.unk4Array[i] = 0;
+            D_8010AF18.unk8Array[i] = 0;
+            D_8010AF18.unkCArray[i] = 0;
+            D_8010AF18.unk10Array[i] = 0;
+            D_8010AF18.unk14[i] = 0;
+            D_8010AF18.unk1C[i] = 0;
+            D_8010AF18.unk24[i] = 0;
+        }
+
+        D_8010AF18.unk28 = 0;
+        D_8010AF18.unk2A = 0;
+        D_8010AF18.transitionState = 0;
+        D_8010AF18.unk2C = 0;
+        D_8010AF18.unk2E = 0;
+    }
+
+ do { var_s0 = 0; if (D_80121B55 > 0) { var_s1 = D_801121E0; do { ; (D_800EC9C4 = var_s1)->unk2C(); var_s0 += 1; var_s1 += 1; } while (var_s0 < D_80121B55); } } while (0);
+    func_8007105C();
+}
 
 void func_8000BEC0(void) {
     ObjectA3E0 *var_s0;
