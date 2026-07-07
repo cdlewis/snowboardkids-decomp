f014436f020a6ebeaaa7633e4ff21c8e644c7876
Chris Lewis <me@chrislewis.au>
2026-07-07 15:15:23 -0400

    Record func_80066ABC decomp attempt

--- src/race_overlay_effects.c
+++ src/race_overlay_effects.c
@@ -144,8 +144,12 @@ extern u8 D_80156609;
 extern u8 D_80121B56;
 extern u8 D_80112130[];
 extern RaceModelEntry *D_800D91E8[];
+extern u32 D_800D9210[];
+extern Gfx D_800D9D00[];
+extern Gfx D_800D9D40[];
 extern GfxCommandDest D_800DEE50;
 extern Gfx *gRegionAllocPtr;
+extern void *D_80156614;
 extern s16 D_80121B50;
 extern void func_80066760(void *);
 extern void func_80066E10(void);
@@ -186,7 +190,126 @@ void func_800668EC(RaceModelListActor *arg0) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/race_overlay_effects/func_800669A0.s")
 
+// func_80066ABC best match: 96.831%
 #pragma GLOBAL_ASM("asm/nonmatchings/race_overlay_effects/func_80066ABC.s")
+
+#ifdef NON_MATCHING
+void func_80066ABC(RaceModelListActor *arg0) {
+    RaceModelEntry *entry;
+    s16 modelIndex;
+    s32 i;
+    register RaceModelListActor *actor;
+    Gfx *temp_v0;
+    void *spA0;
+    void *sp9C;
+    Gfx *temp_v0_2;
+    Gfx *temp_v0_3;
+    Gfx *temp_v0_4;
+    Gfx *temp_v0_5;
+    Gfx *temp_v0_6;
+    Gfx *temp_v0_7;
+    Gfx *temp_v0_8;
+    Gfx *temp_v0_9;
+    Gfx *temp_v0_10;
+    Gfx *temp_v0_11;
+    Gfx *temp_v0_12;
+    Gfx *temp_v0_13;
+    Gfx *temp_v0_14;
+    Gfx *temp_v0_15;
+    Gfx *temp_v0_16;
+    Gfx *temp_v0_17;
+    Gfx *temp_v0_18;
+    Gfx *temp_v0_19;
+
+    actor = arg0;
+    temp_v0 = gRegionAllocPtr++;
+    temp_v0->words.w1 = (u32)D_800D9D00;
+    temp_v0->words.w0 = 0x06000000;
+
+    modelIndex = -1;
+    entry = D_800D91E8[D_80121B50];
+    i = 0;
+    if (entry->modelIndex != -1) {
+        do {
+            if ((entry->enabled != 0) && (func_80049000(&entry->transform) != 0)) {
+                if (modelIndex != entry->modelIndex + actor->modelIndexOffset) {
+                    modelIndex = entry->modelIndex + actor->modelIndexOffset;
+                    func_80045990(func_80043040(D_80112168), (modelIndex + 4) & 0xFFFF, &spA0, &sp9C);
+
+                    temp_v0_2 = gRegionAllocPtr++;
+                    temp_v0_2->words.w0 = 0xFD500000;
+                    temp_v0_2->words.w1 = (u32)spA0;
+
+                    temp_v0_3 = gRegionAllocPtr++;
+                    temp_v0_3->words.w1 = 0x07080200;
+                    temp_v0_3->words.w0 = 0xF5500000;
+
+                    temp_v0_4 = gRegionAllocPtr++;
+                    temp_v0_4->words.w1 = 0;
+                    temp_v0_4->words.w0 = 0xE6000000;
+
+                    temp_v0_5 = gRegionAllocPtr++;
+                    temp_v0_5->words.w1 = 0x0703F800;
+                    temp_v0_5->words.w0 = 0xF3000000;
+
+                    temp_v0_6 = gRegionAllocPtr++;
+                    temp_v0_6->words.w1 = 0;
+                    temp_v0_6->words.w0 = 0xE7000000;
+
+                    temp_v0_7 = gRegionAllocPtr++;
+                    temp_v0_7->words.w0 = 0xF5400200;
+                    temp_v0_7->words.w1 = 0x00080200;
+
+                    temp_v0_8 = gRegionAllocPtr++;
+                    temp_v0_8->words.w0 = 0xF2000000;
+                    temp_v0_8->words.w1 = 0x0003C03C;
+
+                    temp_v0_9 = gRegionAllocPtr++;
+                    temp_v0_9->words.w0 = 0xFD100000;
+                    temp_v0_9->words.w1 = (u32)sp9C;
+
+                    temp_v0_10 = gRegionAllocPtr++;
+                    temp_v0_10->words.w1 = 0;
+                    temp_v0_10->words.w0 = 0xE8000000;
+
+                    temp_v0_11 = gRegionAllocPtr++;
+                    temp_v0_11->words.w0 = 0xF5000100;
+                    temp_v0_11->words.w1 = 0x07000000;
+
+                    temp_v0_12 = gRegionAllocPtr++;
+                    temp_v0_12->words.w1 = 0;
+                    temp_v0_12->words.w0 = 0xE6000000;
+
+                    temp_v0_13 = gRegionAllocPtr++;
+                    temp_v0_13->words.w1 = 0x0703C000;
+                    temp_v0_13->words.w0 = 0xF0000000;
+
+                    temp_v0_14 = gRegionAllocPtr++;
+                    temp_v0_14->words.w1 = 0;
+                    temp_v0_14->words.w0 = 0xE7000000;
+                }
+
+                gSPMatrix(gRegionAllocPtr++, &actor->modelBuffer[i], G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+                gSPMatrix(gRegionAllocPtr++, D_80156614, G_MTX_NOPUSH | G_MTX_MUL | G_MTX_MODELVIEW);
+
+                temp_v0_15 = gRegionAllocPtr++;
+                temp_v0_15->words.w1 = (u32)D_800D9210;
+                temp_v0_15->words.w0 = 0x0400103F;
+
+                temp_v0_16 = gRegionAllocPtr++;
+                temp_v0_16->words.w1 = 0x00060200;
+                temp_v0_16->words.w0 = 0xB1060402;
+            }
+            entry++;
+            i++;
+        } while (entry->modelIndex != -1);
+    }
+
+    temp_v0_19 = gRegionAllocPtr++;
+    temp_v0_19->words.w1 = (u32)D_800D9D40;
+    temp_v0_19->words.w0 = 0x06000000;
+}
+#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/race_overlay_effects/func_80066E10.s")
 
