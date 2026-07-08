7dcdbd6278d5640c972d85473c03402d7403d80c
Chris Lewis <me@chrislewis.au>
2026-07-08 03:38:45 -0400

    Document func_80019FFC partial match

--- src/player_select_ui.c
+++ src/player_select_ui.c
@@ -329,7 +329,44 @@ void func_80019FAC(PlayerSelectWidgetActor *arg0) {
     func_80071824(arg0, func_80019CD8);
 }
 
+// func_80019FFC best match: 97.197%
 #pragma GLOBAL_ASM("asm/nonmatchings/player_select_ui/func_80019FFC.s")
+
+#ifdef NON_MATCHING
+void func_80019FFC(PlayerSelectWidgetActor *arg0) {
+    s32 i;
+    s32 tile;
+    s32 tile2;
+    s32 offset;
+
+    i = 0;
+    tile = 0;
+    do {
+        func_800112F4((s16)(arg0->x + ((i & 3) << 5)), (s16)(arg0->y + ((i / 4) << 5)),
+                      func_80043040(D_80112130.textureHandle), D_800B5B50[(u16)arg0->sprite.spriteIndex][tile], 0,
+                      0x100, 0xA0, 0x49);
+        i++;
+        tile++;
+    } while (i < 0x10);
+
+    tile2 = 0;
+    offset = 0;
+    i = 0x80;
+    do {
+        func_800112F4((s16)(arg0->x + 0x80), (s16)(arg0->y + offset),
+                      func_80043040(D_80112130.textureHandle), D_800B5B50[(u16)arg0->sprite.spriteIndex][tile2 + 16],
+                      0, 0x100, 0xA0, 0x49);
+        func_800112F4((s16)(arg0->x + offset), (s16)(arg0->y + 0x80),
+                      func_80043040(D_80112130.textureHandle), D_800B5B50[(u16)arg0->sprite.spriteIndex][tile2 + 18],
+                      0, 0x100, 0xA0, 0x49);
+        offset += 0x40;
+        tile2++;
+    } while (offset != i);
+
+    func_800112F4((s16)(arg0->x + 0x80), (s16)(arg0->y + 0x80), func_80043040(D_80112130.textureHandle),
+                  D_800B5B50[(u16)arg0->sprite.spriteIndex][20], 0, 0x100, 0xA0, 0x49);
+}
+#endif
 
 void func_8001A270(PlayerSelectWidgetActor *arg0) {
     int state;
