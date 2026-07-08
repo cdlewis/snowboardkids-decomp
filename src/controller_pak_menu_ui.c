21242350b86d24ecc4c3478b61db87706870f15f
Chris Lewis <me@chrislewis.au>
2026-07-08 13:04:23 -0400

    Improve func_8003209C partial match

--- src/controller_pak_menu_ui.c
+++ src/controller_pak_menu_ui.c
@@ -662,41 +662,43 @@ void func_8003205C(ControllerPakDeletePromptActor *arg0) {
     func_80071824(arg0, func_80031F40);
 }
 
-// func_8003209C best match: 89.425% (nonmatchings/func_8003209C-2911448260736516995/base_3.c)
+// func_8003209C best match: 91.922% (nonmatchings/func_8003209C-4033633224288138541/base_3.c)
 #pragma GLOBAL_ASM("asm/nonmatchings/controller_pak_menu_ui/func_8003209C.s")
 
 #ifdef NON_MATCHING
 void func_8003209C(ControllerPakDeletePromptActor *arg0) {
-    s32 xOffset;
-    s32 yOffset;
+    s32 i;
+    s32 j;
     s32 alpha;
+    s32 limit;
 
     func_8000F030(arg0->common.x, arg0->common.y, func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 2, 0x20, 0x20, 0, 0);
     func_8000F030((s16)(arg0->common.x + 0x90), arg0->common.y, func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 4, 0x20, 0x20, 0, 0);
 
-    xOffset = 0;
+    i = 0;
     do {
-        func_8000F030((s16)(arg0->common.x + xOffset + 0x10), arg0->common.y, func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 3, 0x20, 0x20, 0, 0);
-        func_8000F030((s16)(arg0->common.x + xOffset + 0x10), (s16)(arg0->common.y + 0x30), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 8, 0x20, 0x20, 0, 0);
-        xOffset += 0x10;
-    } while (xOffset < 0x80);
+        func_8000F030((s16)(arg0->common.x + i + 0x10), arg0->common.y, func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 3, 0x20, 0x20, 0, 0);
+        func_8000F030((s16)(arg0->common.x + i + 0x10), (s16)(arg0->common.y + 0x30), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 8, 0x20, 0x20, 0, 0);
+        i += 0x10;
+    } while (i < 0x80);
 
     func_8000F030(arg0->common.x, (s16)(arg0->common.y + 0x30), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 7, 0x20, 0x20, 0, 0);
     func_8000F030((s16)(arg0->common.x + 0x90), (s16)(arg0->common.y + 0x30), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 9, 0x20, 0x20, 0, 0);
 
-    yOffset = 0;
+    i = 0;
+    limit = 0x80;
     do {
-        func_8000F030(arg0->common.x, (s16)(arg0->common.y + yOffset + 0x10), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 5, 0x20, 0x20, 0, 0);
-        func_8000F030((s16)(arg0->common.x + 0x90), (s16)(arg0->common.y + yOffset + 0x10), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 6, 0x20, 0x20, 0, 0);
+        func_8000F030(arg0->common.x, (s16)(arg0->common.y + i + 0x10), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 5, 0x20, 0x20, 0, 0);
+        func_8000F030((s16)(arg0->common.x + 0x90), (s16)(arg0->common.y + i + 0x10), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 6, 0x20, 0x20, 0, 0);
 
-        xOffset = 0;
+        j = 0;
         do {
-            func_8000F030((s16)(arg0->common.x + xOffset + 0x10), (s16)(arg0->common.y + yOffset + 0x10), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 0xB, 0x20, 0x20, 0, 0);
-            xOffset += 0x10;
-        } while (xOffset != 0x80);
+            func_8000F030((s16)(arg0->common.x + j + 0x10), (s16)(arg0->common.y + i + 0x10), func_80043040(CONTROLLER_PAK_DELETE_PANEL_HANDLE), 0xB, 0x20, 0x20, 0, 0);
+            j += 0x10;
+        } while (j != limit);
 
-        yOffset += 0x10;
-    } while (yOffset != 0x20);
+        i += 0x10;
+    } while (i != 0x20);
 
     func_80013154((s16)(arg0->common.x + 0x1C), (s16)(arg0->common.y + 4), D_800B8070, 1, 0x100, 0);
 
@@ -705,7 +707,7 @@ void func_8003209C(ControllerPakDeletePromptActor *arg0) {
         alpha = 0x100;
     }
 
-    func_8000F8AC((s16)(arg0->common.x + 0x2C), (s16)(arg0->common.y + 0x18), func_80043040(D_80112178), 0x17, 0x20, 0x20, 0, alpha, 0);
+    func_8000F8AC((s16)(arg0->common.x + 0x2C), (s16)(arg0->common.y + 0x18), func_80043040(D_80112130[0x24]), 0x17, 0x20, 0x20, 0, alpha, 0);
 
     if (alpha == 0x100) {
         alpha = 0x60;
@@ -713,8 +715,8 @@ void func_8003209C(ControllerPakDeletePromptActor *arg0) {
         alpha = 0x100;
     }
 
-    func_8000F8AC((s16)(arg0->common.x + 0x2C), (s16)(arg0->common.y + 0x28), func_80043040(D_80112178), 0x18, 0x20, 0x20, 0, alpha, 0);
-    func_8000F8AC((s16)(arg0->common.x + 0x2C), (s16)(arg0->common.y + (arg0->selectedOption * 0x10) + 0x18), func_80043040(D_80112178), 0x12, 0x20, 0x20, 0, arg0->scale, 0);
+    func_8000F8AC((s16)(arg0->common.x + 0x2C), (s16)(arg0->common.y + 0x28), func_80043040(D_80112130[0x24]), 0x18, 0x20, 0x20, 0, alpha, 0);
+    func_8000F8AC((s16)(arg0->common.x + 0x2C), (s16)(arg0->common.y + (arg0->selectedOption * 0x10) + 0x18), func_80043040(D_80112130[0x24]), 0x12, 0x20, 0x20, 0, arg0->scale, 0);
 }
 #endif
 
