73a538022b12e29ac987bd92c285aaed253cba35
Chris Lewis <me@chrislewis.au>
2026-07-08 06:38:14 -0400

    Decompile func_8002015C

--- src/character_select_ui.c
+++ src/character_select_ui.c
@@ -1110,7 +1110,39 @@ void func_80020118(CharacterSelectWidgetActor *arg0) {
     func_80071824(arg0, func_8001FF08);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/character_select_ui/func_8002015C.s")
+void func_8002015C(CharacterSelectWidgetActor *arg0) {
+    s32 shouldDraw;
+    s32 i;
+    s32 tileOffset;
+    register s32 offset;
+
+    tileOffset = 0;
+    shouldDraw = 1;
+    for (i = 0; i < 16; i++, tileOffset++) {
+        func_800112F4(arg0->x + ((i & 3) << 5), arg0->y + ((i / 4) << 5), func_80043040(D_80112130.textureHandle),
+                      D_800B5FC0[(u16)arg0->sprite.index].center[tileOffset], 0, 0x100, 0xA0, 0x49);
+    }
+
+    if (shouldDraw) {
+        tileOffset = 0;
+        i = 0x80;
+    }
+    offset = 0;
+    do {
+        func_800112F4(arg0->x + 0x80, arg0->y + offset, func_80043040(D_80112130.textureHandle),
+                      D_800B5FC0[(u16)arg0->sprite.index].right[tileOffset], 0, 0x100, 0xA0, 0x49);
+        func_800112F4(arg0->x + offset, arg0->y + 0x80, func_80043040(D_80112130.textureHandle),
+                      D_800B5FC0[(u16)arg0->sprite.index].bottom[tileOffset], 0, 0x100, 0xA0, 0x49);
+        i = 0x80;
+        offset += 0x40;
+        tileOffset++;
+    } while (offset != i);
+    i++;
+    i--;
+
+    func_800112F4(arg0->x + 0x80, arg0->y + 0x80, func_80043040(D_80112130.textureHandle),
+                  D_800B5FC0[(u16)arg0->sprite.index].corner, 0, 0x100, 0xA0, 0x49);
+}
 
 void func_800203D0(CharacterSelectWidgetActor *arg0) {
     int state;
