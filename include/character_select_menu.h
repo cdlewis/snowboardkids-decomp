b9f6eb8fdc302c344266baf73de76c16d4485ed8
Chris Lewis <me@chrislewis.au>
2026-07-08 11:45:35 -0400

    Add character select menu header

---
+++ include/character_select_menu.h
@@ -0,0 +1,11 @@
+#ifndef CHARACTER_SELECT_MENU_H
+#define CHARACTER_SELECT_MENU_H
+
+#include "common.h"
+
+void func_80004960(void);
+void func_80004CC8(void);
+void func_80005290(void);
+void func_80005468(void);
+
+#endif
