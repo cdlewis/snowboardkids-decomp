281787ed45467153e77114f6eacfc4a32c3c71ca
Chris Lewis <me@chrislewis.au>
2026-07-08 11:52:20 -0400

    Declare controller pak delete flow functions

---
+++ include/controller_pak_delete_flow.h
@@ -0,0 +1,9 @@
+#ifndef CONTROLLER_PAK_DELETE_FLOW_H
+#define CONTROLLER_PAK_DELETE_FLOW_H
+
+void func_80008D60(void);
+void func_80008F2C(void);
+void func_800095DC(void);
+void func_80009690(void);
+
+#endif
