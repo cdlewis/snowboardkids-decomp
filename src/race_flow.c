3697223f042e7c6cf69a9ab9ac45de7ca0554d28
Chris Lewis <me@chrislewis.au>
2026-07-08 13:46:43 -0400

    Declare game setup menu public interface in game_setup_menu.h
    Add include/game_setup_menu.h with prototypes for the functions defined in
    src/game_setup_menu.c (func_80003140, func_8000337C, func_800035F8,
    func_80003798, __dummy, func_80004164, func_800045D8). func_800045D8 takes
    a single s32 player index argument; the rest take none.
    
    Include the new header from game_setup_menu.c and update race_flow.c to use
    it in place of its local 'extern void func_80003140(void);' declaration.

--- src/race_flow.c
+++ src/race_flow.c
@@ -9,6 +9,7 @@
 #include "controller_pak_message_flow.h"
 #include "controller_pak_menu_flow.h"
 #include "course_select_menu.h"
+#include "game_setup_menu.h"
 
 #define COURSE_GRID_ENTRY_FREE -1
 #define COURSE_GRID_ENTRY_END -2
@@ -119,7 +120,6 @@ extern s16 func_80042D58(s32);
 extern s32 *func_80043040(s16);
 extern void func_80000A40(s32);
 extern void func_80001C30(void);
-extern void func_80003140(void);
 extern void func_800055EC(void);
 extern void func_800086EC(void);
 extern void func_8000C280(void);
