a2d31e6d5aab41ee778043561112ec61d11c5259
Chris Lewis <me@chrislewis.au>
2026-07-09 14:09:35 -0400

    Decompile func_8005FED0

--- src/race_ui_effects.c
+++ src/race_ui_effects.c
@@ -874,6 +874,8 @@ extern const char D_800E1474[];
 extern const char D_800E1484[];
 extern const char D_800E1494[];
 extern const char D_800E14A8[];
+extern Gfx D_800D6120[];
+extern Gfx D_800D6190[];
 extern u32 D_800DEFF8[];
 extern Gfx *gRegionAllocPtr;
 extern RaceUiAssetEntry D_800D5FF4[];
@@ -3073,31 +3075,9 @@ void func_8005FBA8(RaceUiAnimatedTextActor *arg0) {
     }
 }
 
-// func_8005FED0 best match: 99.204% (nonmatchings/func_8005FED0-2127290767680699791/base_8.c)
-#pragma GLOBAL_ASM("asm/nonmatchings/race_ui_effects/func_8005FED0.s")
-
-#ifdef NON_MATCHING
 void func_8005FED0(RaceUiTextParticleActor *arg0) {
     volatile u8 padding[4];
     RaceUiTrailCopyBlock sp64;
-    Gfx *temp_v0;
-    Gfx *temp_v0_2;
-    Gfx *temp_v0_3;
-    Gfx *temp_v0_4;
-    Gfx *temp_v0_5;
-    Gfx *temp_v0_6;
-    Gfx *temp_v0_7;
-    Gfx *temp_v0_8;
-    Gfx *temp_v0_9;
-    Gfx *temp_v0_10;
-    Gfx *temp_v0_11;
-    Gfx *temp_v0_12;
-    Gfx *temp_v0_13;
-    Gfx *temp_v0_14;
-    Gfx *temp_v0_15;
-    Gfx *temp_v0_16;
-    Gfx *temp_v0_17;
-    Gfx *temp_v0_18;
 
     if (D_80156609 != 0) {
         arg0->matrixDirty = 1;
@@ -3112,66 +3092,17 @@ void func_8005FED0(RaceUiTextParticleActor *arg0) {
         arg0->matrix = func_8004885C(&sp64);
     }
 
-    do {
-        if (arg0->matrix != NULL) {
-            temp_v0 = gRegionAllocPtr++;
-            temp_v0->words.w0 = 0x06000000;
-            temp_v0->words.w1 = (u32)D_800D6190;
-            temp_v0_2 = gRegionAllocPtr++;
-            temp_v0_2->words.w0 = 0xFD500000;
-            temp_v0_2->words.w1 = (u32)arg0->images[arg0->timer >> 2];
-            temp_v0_3 = gRegionAllocPtr++;
-            temp_v0_3->words.w0 = 0xF5500000;
-            temp_v0_3->words.w1 = 0x07080200;
-            temp_v0_4 = gRegionAllocPtr++;
-            temp_v0_4->words.w1 = 0;
-            temp_v0_4->words.w0 = 0xE6000000;
-            temp_v0_5 = gRegionAllocPtr++;
-            temp_v0_5->words.w0 = 0xF3000000;
-            temp_v0_5->words.w1 = 0x070FF400;
-            temp_v0_6 = gRegionAllocPtr++;
-            temp_v0_6->words.w1 = 0;
-            temp_v0_6->words.w0 = 0xE7000000;
-            temp_v0_7 = gRegionAllocPtr++;
-            temp_v0_7->words.w1 = 0x00080200;
-            temp_v0_7->words.w0 = 0xF5400400;
-            temp_v0_8 = gRegionAllocPtr++;
-            temp_v0_8->words.w0 = 0xF2000000;
-            temp_v0_8->words.w1 = 0x0007C07C;
-            temp_v0_9 = gRegionAllocPtr++;
-            temp_v0_9->words.w0 = 0xFD100000;
-            temp_v0_9->words.w1 = (u32)arg0->palettes[arg0->timer >> 2];
-            temp_v0_10 = gRegionAllocPtr++;
-            temp_v0_10->words.w1 = 0;
-            temp_v0_10->words.w0 = 0xE8000000;
-            temp_v0_11 = gRegionAllocPtr++;
-            temp_v0_11->words.w0 = 0xF5000100;
-            temp_v0_11->words.w1 = 0x07000000;
-            temp_v0_12 = gRegionAllocPtr++;
-            temp_v0_12->words.w1 = 0;
-            temp_v0_12->words.w0 = 0xE6000000;
-            temp_v0_13 = gRegionAllocPtr++;
-            temp_v0_13->words.w0 = 0xF0000000;
-            temp_v0_13->words.w1 = 0x0703C000;
-            temp_v0_14 = gRegionAllocPtr++;
-            temp_v0_14->words.w1 = 0;
-            temp_v0_14->words.w0 = 0xE7000000;
-            temp_v0_15 = gRegionAllocPtr++;
-            temp_v0_15->words.w0 = 0x01020040;
-            temp_v0_15->words.w1 = (u32)arg0->matrix;
-            temp_v0_16 = gRegionAllocPtr++;
-            temp_v0_16->words.w0 = 0x01000040;
-            temp_v0_16->words.w1 = D_80156614;
-            temp_v0_17 = gRegionAllocPtr++;
-            temp_v0_17->words.w0 = 0x0400103F;
-            temp_v0_17->words.w1 = (u32)D_800D6120;
-            temp_v0_18 = gRegionAllocPtr++;
-            temp_v0_18->words.w1 = 0x00060200;
-            temp_v0_18->words.w0 = 0xB1060402;
-        }
-    } while (0);
+    if (arg0->matrix != NULL) {
+        gSPDisplayList(gRegionAllocPtr++, D_800D6190);
+        gDPLoadTextureBlock_4b(gRegionAllocPtr++, arg0->images[arg0->timer >> 2], G_IM_FMT_CI, 0x20, 0x20,
+                               0, G_TX_CLAMP, G_TX_CLAMP, 0, 0, G_TX_NOLOD, G_TX_NOLOD);
+        gDPLoadTLUT_pal16(gRegionAllocPtr++, 0, arg0->palettes[arg0->timer >> 2]);
+        gSPMatrix(gRegionAllocPtr++, arg0->matrix, G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPMatrix(gRegionAllocPtr++, D_80156614, G_MTX_NOPUSH | G_MTX_MUL | G_MTX_MODELVIEW);
+        RACE_UI_GSP_VERTEX_F3DEX(gRegionAllocPtr++, D_800D6120, 4, 0);
+        RACE_UI_GSP1QUADRANGLE_F3DEX(gRegionAllocPtr++, 2, 1, 0, 3, 3);
+    }
 }
-#endif
 
 void func_800601F8(void *arg0) {
     s32 temp;
