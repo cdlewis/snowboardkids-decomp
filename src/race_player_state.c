5a496b98b2012d7bb2ed1987a404cd4488a24b66
Chris Lewis <me@chrislewis.au>
2026-07-09 08:37:22 -0400

    Decompile func_80092468

--- src/race_player_state.c
+++ src/race_player_state.c
@@ -1201,7 +1201,66 @@ void func_80092368(RaceInputPlayer *player) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/race_player_state/func_80092468.s")
+void func_80092468(RaceInputPlayer *player) {
+    s16 updateTimer;
+    s32 yVel;
+    s32 timer;
+
+    if (player->updateState == 0) {
+        func_80081E40(player, 4);
+        player->updateState++;
+        player->stateFlags |= 0x200;
+        player->stateTimer = 0;
+        func_8008F1B4(player);
+        func_80081E40(player, 0x19);
+        player->updateTimer = 0;
+    }
+
+    func_80082EC0(player);
+    func_8008B408(player, player->unk254, 0);
+    player->unk40.y -= player->unk264;
+    func_8008B508(&player->unk40, player);
+
+    yVel = player->unk40.y;
+    player->posX += player->unk40.x;
+    player->posY += yVel;
+    player->posZ += player->unk40.z;
+    player->unk74 = yVel;
+
+    if (player->stateFlags & 0x400) {
+        player->unk6E = (func_80097AE8(player->unk7E) * -0x1800) / 4096;
+    } else {
+        player->unk6E = ((func_80097AE8(player->unk7E) * 6) * 1024) / 4096;
+    }
+
+    player->stateTimer += 0x16;
+    player->updateTimer++;
+    updateTimer = player->updateTimer;
+    if (updateTimer == 8) {
+        func_80081E40(player, 0x1A);
+        updateTimer = player->updateTimer;
+    }
+    if (updateTimer == 0xF) {
+        func_80081E40(player, 0x1B);
+        updateTimer = player->updateTimer;
+    }
+    if (updateTimer == 0x1E) {
+        func_80081E40(player, 0x1C);
+    }
+
+    if (player->stateTimer >= 0x401) {
+        player->stateTimer = 0x400;
+    }
+
+    timer = player->stateTimer;
+    player->stateFlags |= 2;
+    if (timer < 0x3D0) {
+        player->stateFlags |= 0x800;
+        if ((player->soundDisabled == 0) && (D_801235B0 & 1)) {
+            func_800716A4(func_80050E80, 5, 2, (u16) player->playerIndex);
+        }
+    }
+}
 
 void func_80092674(RaceInputPlayer *player) {
     s16 updateState;
