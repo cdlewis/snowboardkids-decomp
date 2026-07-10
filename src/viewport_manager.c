ebbb2342c20915f0ccd5d2b8ce375027d8f56950
Chris Lewis <me@chrislewis.au>
2026-07-10 02:32:15 -0400

    Document func_80070C64 partial decomp

--- src/viewport_manager.c
+++ src/viewport_manager.c
@@ -277,7 +277,69 @@ void func_80070A70(s32 arg0, s32 arg1, s32 arg2, s32 arg3, u16 arg4, u16 arg5, u
 }
 #endif
 
+// func_80070C64 best match: 90.016%
+
 #pragma GLOBAL_ASM("asm/nonmatchings/viewport_manager/func_80070C64.s")
+
+#ifdef NON_MATCHING
+extern f32 D_800E153C;
+extern f32 D_800E1540;
+
+void func_80070C64(s32 arg0, s32 arg1, s32 arg2, u16 arg3, u16 arg4, u16 arg5, u16 arg6, s32 arg7) {
+    ViewportState *viewport;
+    ViewportState *savedViewport;
+    s32 halfHeight;
+    s32 halfWidth;
+
+    viewport = &D_801124B0[arg0];
+    viewport->viewportTranslateX = arg1 * 4;
+    viewport->active = 1;
+    viewport->viewportTranslateY = arg2 * 4;
+    viewport->viewportScaleX = arg5 * 2;
+    viewport->viewportScaleY = arg6 * 2;
+
+    halfWidth = arg3 / 2;
+    viewport->right = halfWidth + arg1;
+    viewport->left = arg1 - halfWidth;
+    halfHeight = arg4 / 2;
+    viewport->top = arg2 - halfHeight;
+    viewport->bottom = halfHeight + arg2;
+    viewport->right = viewport->right;
+    viewport->screenBoundsValid = 1;
+    viewport->left = viewport->left;
+    viewport->top = viewport->top;
+    viewport->bottom = viewport->bottom;
+
+    if (viewport->right < 0) {
+        viewport->screenBoundsValid = 0;
+    }
+    if (viewport->bottom < 0) {
+        viewport->screenBoundsValid = 0;
+    }
+    if (viewport->left >= 0x140) {
+        viewport->screenBoundsValid = 0;
+    }
+    if (viewport->top >= 0xF0) {
+        viewport->screenBoundsValid = 0;
+    }
+    if (viewport->left < 0) {
+        viewport->left = 0;
+    }
+    if (viewport->top < 0) {
+        viewport->top = 0;
+    }
+    if (viewport->right >= 0x140) {
+        viewport->right = 0x13F;
+    }
+    if (viewport->bottom >= 0xF0) {
+        viewport->bottom = 0xEF;
+    }
+
+    savedViewport = viewport;
+    guPerspective(&viewport->projection, &viewport->perspectiveNorm, 70.0f, arg7, 10.0f, D_800E153C, 0.5f);
+    guPerspective(&savedViewport->overlayProjection, &savedViewport->overlayPerspectiveNorm, 70.0f, arg7, 10.0f, D_800E1540, 0.5f);
+}
+#endif
 
 void func_80070E58(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
     ViewportState *viewport = &D_801124B0[arg0];
