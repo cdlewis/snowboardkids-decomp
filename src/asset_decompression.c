74fa5066b05aaaa3f3a4c04ee347a84c15f0e320
Chris Lewis <me@chrislewis.au>
2026-07-10 04:01:55 -0400

    Document func_800433D8 decomp attempt

--- src/asset_decompression.c
+++ src/asset_decompression.c
@@ -157,7 +157,190 @@ void func_8004331C(s16 arg0) {
     }
 }
 
+// func_800433D8 best match: 62.649%
 #pragma GLOBAL_ASM("asm/nonmatchings/asset_decompression/func_800433D8.s")
+
+#ifdef NON_MATCHING
+void func_800433D8(u8 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    HuffmanNode *temp_v0;
+    HuffmanNode *temp_v0_2;
+    HuffmanNode *temp_v1;
+    HuffmanNode *temp_v1_2;
+    HuffmanNode *temp_v1_3;
+    s16 temp_a1;
+    s16 temp_s3_2;
+    s16 temp_s6_2;
+    s16 temp_v0_3;
+    s16 temp_v0_4;
+    s16 temp_v0_5;
+    s16 var_s0_2;
+    s16 var_s0_3;
+    s16 var_s0_4;
+    s32 temp_a2;
+    s32 temp_a2_2;
+    s32 temp_at;
+    s32 temp_s2;
+    s32 var_a0;
+    u8 *var_a1;
+    u8 *var_s1;
+    s32 var_s2;
+    s32 var_t2;
+    s32 var_v0;
+    s8 temp_t3;
+    u8 *var_s1_2;
+    u8 *var_v1;
+    u8 temp_s3;
+    u8 temp_s6;
+    u8 temp_t7;
+    u8 temp_t8;
+    u8 var_s0;
+    u8 *var_a1_2;
+
+    D_800D4010 = -1;
+    D_800D4014 = -1;
+    D_800D4018 = 0;
+    D_80112128 = 0;
+    var_s2 = 0;
+    var_s1 = (u8 *) arg1;
+loop_1:
+    temp_s3 = *var_s1;
+    temp_s2 = var_s2 + 1;
+    var_s1_2 = var_s1 + 1;
+    if (temp_s2 == 1) {
+block_3:
+        temp_s6 = var_s1[1];
+        var_s2 = temp_s2 + 1;
+        temp_at = (s32) temp_s6 < (s32) temp_s3;
+        var_s1 = var_s1_2 + 1;
+        var_s0 = temp_s3;
+        if (temp_at == 0) {
+            do {
+                temp_t7 = *var_s1;
+                var_s2 += 1;
+                var_s1 += 1;
+                temp_v0 = &D_80110928[D_80112128];
+                temp_v0->left = -1;
+                temp_v0->right = -1;
+                temp_v0->value = (s16) var_s0;
+                temp_v0->weight = (s16) temp_t7;
+                func_800431B0((s16) D_80112128);
+                var_s0 += 1;
+                D_80112128 += 1;
+            } while ((temp_s6 + 1) != var_s0);
+        }
+        goto loop_1;
+    }
+    if (temp_s3 != 0) {
+        goto block_3;
+    }
+loop_7:
+    if (D_800D4018 >= 2) {
+        temp_s3_2 = D_800D4014;
+        func_8004331C(temp_s3_2);
+        temp_s6_2 = D_800D4014;
+        func_8004331C(temp_s6_2);
+        temp_v0_2 = &D_80110928[D_80112128];
+        temp_v0_2->weight = D_80110928[temp_s3_2].weight + D_80110928[temp_s6_2].weight;
+        temp_v0_2->left = temp_s6_2;
+        temp_v0_2->right = temp_s3_2;
+        temp_v0_2->value = -1;
+        func_800431B0((s16) D_80112128);
+        D_80112128 += 1;
+        goto loop_7;
+    }
+    var_a0 = 0;
+    var_t2 = 0;
+    if (arg0 == 0) {
+        var_a1 = (u8 *) arg2;
+        do {
+            var_s0_2 = D_80112128 - 1;
+loop_13:
+            temp_v1 = &D_80110928[var_s0_2];
+            temp_v0_3 = temp_v1->value;
+            if (temp_v0_3 == -1) {
+                if (var_a0 == 8) {
+                    var_s1 += 1;
+                    var_a0 = 0;
+                }
+                var_v0 = *var_s1 & (1 << (7 - var_a0));
+                var_a0 += 1;
+                if (var_v0 == 0) {
+                    var_s0_2 = temp_v1->left;
+                } else {
+                    var_s0_2 = temp_v1->right;
+                }
+                goto loop_13;
+            }
+            *var_a1 = (s8) (temp_v0_3 & 0xFF);
+            var_t2 += 1;
+            var_a1 += 1;
+        } while (var_t2 != arg3);
+    } else {
+loop_23:
+        temp_a1 = D_80112128 - 1;
+        var_s0_3 = temp_a1;
+loop_24:
+        temp_v1_2 = &D_80110928[var_s0_3];
+        temp_v0_4 = temp_v1_2->value;
+        temp_a2 = temp_v0_4 & 0xFF;
+        if (temp_v0_4 == -1) {
+            if (var_a0 == 8) {
+                var_s1 += 1;
+                var_a0 = 0;
+            }
+            var_v0 = *var_s1 & (1 << (7 - var_a0));
+            var_a0 += 1;
+            if (var_v0 == 0) {
+                var_s0_3 = temp_v1_2->left;
+            } else {
+                var_s0_3 = temp_v1_2->right;
+            }
+            goto loop_24;
+        }
+        var_s0_4 = temp_a1;
+loop_31:
+        temp_v1_3 = &D_80110928[var_s0_4];
+        temp_v0_5 = temp_v1_3->value;
+        if (temp_v0_5 == -1) {
+            if (var_a0 == 8) {
+                var_s1 += 1;
+                var_a0 = 0;
+            }
+            var_v0 = *var_s1 & (1 << (7 - var_a0));
+            var_a0 += 1;
+            if (var_v0 == 0) {
+                var_s0_4 = temp_v1_3->left;
+            } else {
+                var_s0_4 = temp_v1_3->right;
+            }
+            goto loop_31;
+        }
+        temp_t3 = temp_v0_5 & 0xFF;
+        if (temp_a2 == 0) {
+            ((u8 *) arg2)[var_t2] = temp_t3;
+            var_t2 += 1;
+        } else {
+            temp_a2_2 = (temp_a2 >> 4) & 0xF;
+            var_v0 = 0;
+            if (temp_a2_2 > 0) {
+                var_a1_2 = (u8 *) arg2 + var_t2;
+                var_v1 = (u8 *) arg2 + (var_t2 - (((temp_a2 << 8) | temp_t3) & 0xFFF));
+                do {
+                    temp_t8 = *var_v1;
+                    var_v0 += 1;
+                    var_t2 += 1;
+                    var_a1_2 += 1;
+                    var_v1 += 1;
+                    var_a1_2[-1] = temp_t8;
+                } while (var_v0 < temp_a2_2);
+            }
+        }
+        if (var_t2 < arg3) {
+            goto loop_23;
+        }
+    }
+}
+#endif
 
 void func_800437F0(void *arg0, void *arg1, s32 arg2) {
     s16 *sp28;
