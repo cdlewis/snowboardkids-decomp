173cd2f5d91dff469b643275a13e1cc62e69088f
Chris Lewis <me@chrislewis.au>
2026-07-11 06:46:11 -0400

    Rename ending credits Slash actor

---
+++ include/ending_credits_slash.h
@@ -0,0 +1,86 @@
+#ifndef ENDING_CREDITS_SLASH_H
+#define ENDING_CREDITS_SLASH_H
+
+#include "common.h"
+
+typedef struct EndingCreditsSlash EndingCreditsSlash;
+
+void noopEndingCreditsSlash(void);
+void updateEndingSlashFinalPose(EndingCreditsSlash *arg0);
+void updateEndingSlashStartFinalPose(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase40(EndingCreditsSlash *arg0);
+void updateEndingSlashPhase40Prep(EndingCreditsSlash *arg0);
+void updateEndingSlashWaitPhase3FAnim(EndingCreditsSlash *arg0);
+void updateEndingSlashSpinUntilPhase3F(EndingCreditsSlash *arg0);
+void updateEndingSlashTumbleToPhase3C(EndingCreditsSlash *arg0);
+void updateEndingSlashExitUntilPhase3B(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase3A(s32 arg0);
+void updateEndingSlashSlowRotationWipe(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideToCenter(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase37(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideLeftFromFarRight(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase34(EndingCreditsSlash *arg0);
+void updateEndingSlashDashOffLeft(EndingCreditsSlash *arg0);
+void updateEndingSlashVanishRetreat(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase2F(void *arg0);
+void updateEndingSlashDriftAfterPhase2D(EndingCreditsSlash *arg0);
+void updateEndingSlashRunLeftThenPhase2B(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideFarLeft(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase2A(s32 arg0);
+void updateEndingSlashSlideRightToPose(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase28(s32 arg0);
+void updateEndingSlashSlideLeftToMarker(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideRightToMarker(EndingCreditsSlash *arg0);
+void startEndingSlashPhase27Slide(void *arg0);
+void updateEndingSlashSlideLeftSetPhase26(EndingCreditsSlash *arg0);
+void noopEndingCreditsSlashAlt(void);
+void updateEndingSlashDiagonalSlideSetPhase24(EndingCreditsSlash *arg0);
+void waitEndingSlashBeforeDiagonalSlide(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideRightSetPhase23(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase22Aura(void *arg0);
+void updateEndingSlashExitLeftAfterPhase22(EndingCreditsSlash *arg0);
+void updateEndingSlashAfterVanishWait(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase1E(void *arg0);
+void updateEndingSlashRepeatAnimThenVanish(EndingCreditsSlash *arg0);
+void startEndingSlashRepeatAnim(s32 arg0);
+void updateEndingSlashWaitBeforeRepeatAnim(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase1B(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase1A(void *arg0);
+void updateEndingSlashHandshakeLoopThird(EndingCreditsSlash *arg0);
+void updateEndingSlashHandshakeLoopSecond(EndingCreditsSlash *arg0);
+void updateEndingSlashHandshakeLoopFirst(EndingCreditsSlash *arg0);
+void startEndingSlashHandshakeLoop(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase19(void *arg0);
+void updateEndingSlashSlideLeftUntilPhase18(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase17(s32 arg0);
+void updateEndingSlashSlideRightToIdle(EndingCreditsSlash *arg0);
+void startEndingSlashIdleAfterAnim(s32 arg0);
+void updateEndingSlashWaitBeforeIdleAnim2(EndingCreditsSlash *arg0);
+void updateEndingSlashWaitBeforeIdleAnim1(EndingCreditsSlash *arg0);
+void updateEndingSlashHopLeftAnim(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideRightBurst(EndingCreditsSlash *arg0);
+void waitEndingSlashRotationPhase14(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideLeftSlowRotation(EndingCreditsSlash *arg0);
+void updateEndingSlashWaitRotationStepTen(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase12(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideRightSetPhase11(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase10Aura(EndingCreditsSlash *arg0);
+void updateEndingSlashExitRightSetPhase0E(EndingCreditsSlash *arg0);
+void updateEndingSlashVanishBeforeExitRight(EndingCreditsSlash *arg0);
+void startEndingSlashVanishBeforeExitRight(s32 arg0);
+void updateEndingSlashWaitRisingStar(EndingCreditsSlash *arg0);
+void waitEndingSlashBeforeRisingStar(EndingCreditsSlash *arg0);
+void updateEndingSlashRepeatAnim17(EndingCreditsSlash *arg0);
+void startEndingSlashRepeatAnim17(s32 arg0);
+void waitEndingSlashPhase0D(EndingCreditsSlash *arg0);
+void updateEndingSlashAfterPhase0DAnim(EndingCreditsSlash *arg0);
+void updateEndingSlashLongWaitSetShadow(EndingCreditsSlash *arg0);
+void updateEndingSlashSlideLeftToAura(EndingCreditsSlash *arg0);
+void updateEndingSlashWaitPhase5To7(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase5(s32 arg0);
+void waitEndingSlashPhase3(EndingCreditsSlash *arg0);
+void updateEndingSlashEnterFromRight(EndingCreditsSlash *arg0);
+void waitEndingSlashPhase2(void *arg0);
+void initEndingCreditsSlash(EndingCreditsSlash *arg0);
+
+#endif
--- src/race_to_main_menu_transition.c
+++ src/race_to_main_menu_transition.c
@@ -9,7 +9,7 @@
 #include "ending_credits_ui.h"
 #include "menu_transition_effects.h"
 #include "race_start_transition.h"
-#include "ending_credits_actor_0.h"
+#include "ending_credits_slash.h"
 #include "main_menu_scene_actor_1.h"
 #include "main_menu_scene_actor_2.h"
 #include "main_menu_scene_actor_3.h"
@@ -139,7 +139,7 @@ void func_8000D590(void) {
         } else {
             setCurrentGameTaskCallback(func_8000D690, 0);
             createCallbackTask((CallbackTaskCallback) initEndingCreditsPageTextActor, 0, 0x64);
-            createCallbackTask((CallbackTaskCallback) initEndingCreditsActor0, 0, 0x64);
+            createCallbackTask((CallbackTaskCallback) initEndingCreditsSlash, 0, 0x64);
             createCallbackTask((CallbackTaskCallback) func_800362E8, 0, 0x64);
             createCallbackTask((CallbackTaskCallback) func_80036FB4, 0, 0x64);
             createCallbackTask((CallbackTaskCallback) func_80039440, 0, 0x64);
