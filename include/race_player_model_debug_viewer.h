a98b35d9d61162269e090a967f581afd2c6f75b0
Chris Lewis <me@chrislewis.au>
2026-07-11 19:17:52 -0400

    Rename race player model debug viewer

---
+++ include/race_player_model_debug_viewer.h
@@ -0,0 +1,10 @@
+#ifndef RACE_PLAYER_MODEL_DEBUG_VIEWER_H
+#define RACE_PLAYER_MODEL_DEBUG_VIEWER_H
+
+#include "common.h"
+
+void initRacePlayerModelDebugViewer(void);
+void drawRacePlayerModelDebugViewerMotionNumber(s32 arg0);
+void updateRacePlayerModelDebugViewer(void);
+
+#endif
