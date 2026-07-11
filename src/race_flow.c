26768f72983614af3e45f1b22a6a0dacd100a4ce
Chris Lewis <me@chrislewis.au>
2026-07-11 02:38:44 -0400

    Rename replay save prompt flow symbols

--- src/race_flow.c
+++ src/race_flow.c
@@ -7,7 +7,7 @@
 #include "character_select_menu.h"
 #include "race_character_select_menu.h"
 #include "controller_pak_score_delete_flow.h"
-#include "controller_pak_replay_save_message_flow.h"
+#include "controller_pak_replay_save_prompt_flow.h"
 #include "controller_pak_file_delete_flow.h"
 #include "course_select_menu.h"
 #include "game_setup_menu.h"
@@ -291,7 +291,7 @@ void func_80072D54(void) {
 
 void func_80072D98(void) {
     func_8009956C(&func_80072DDC, 0);
-    func_800994F4(4, &initControllerPakReplaySaveMessageFlow, 0x64);
+    func_800994F4(4, &initControllerPakReplaySavePromptFlow, 0x64);
     func_80099614(0);
 }
 
