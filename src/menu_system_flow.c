60617eae7768a2487b0a85a4f4393dbf8827694b
Chris Lewis <me@chrislewis.au>
2026-07-11 09:24:20 -0400

    Rename title screen UI symbols

--- src/menu_system_flow.c
+++ src/menu_system_flow.c
@@ -10,7 +10,7 @@
 #include "main_menu_scene_model.h"
 #include "main_menu_scene_model_renderer.h"
 #include "main_menu_race_flow.h"
-#include "main_menu_title_ui.h"
+#include "title_screen_ui.h"
 #include "race_camera.h"
 #include "race_flow.h"
 #include "race_intro_transition.h"
@@ -973,12 +973,12 @@ void initMainMenu(void) {
     gCurrentGameTask->timer = 0x4B0;
     initCallbackTaskScheduler(0);
     if (gConnectedControllerCount != 0) {
-        createCallbackTask(&initMainMenuTitleCursor, 0, 0x64);
+        createCallbackTask(&initTitleScreenMenuCursor, 0, 0x64);
     } else {
         createCallbackTask(&updateTitleStartPrompt, 0, 0x64);
     }
-    createCallbackTask(&initMainMenuTitleOptions, 0, 0x64);
-    createCallbackTask(&initMainMenuTitleLogo, 0, 0x64);
+    createCallbackTask(&initTitleScreenMenuOptions, 0, 0x64);
+    createCallbackTask(&initTitleScreenLogo, 0, 0x64);
     createCallbackTaskWithUserId(&initTitleMenuSparkle, 0, 0x64, 0);
     createCallbackTaskWithUserId(initTitleMenuBoardModels, 0, 0x64, 0);
     setBootFadeColor(0x20, 0x40, 0x50);
