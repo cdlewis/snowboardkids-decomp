d625aee84f4879e70c9cf199dd55e905023884c7
Chris Lewis <me@chrislewis.au>
2026-07-11 05:30:30 -0400

    Rename ending sequence credits module

---
+++ include/ending_sequence_credits.h
@@ -0,0 +1,10 @@
+#ifndef ENDING_SEQUENCE_CREDITS_H
+#define ENDING_SEQUENCE_CREDITS_H
+
+#include "common.h"
+
+typedef struct EndingSequenceCreditsTextActor EndingSequenceCreditsTextActor;
+
+void initEndingSequenceCreditsTextActor(EndingSequenceCreditsTextActor *arg0);
+
+#endif
