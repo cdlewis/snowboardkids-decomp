d9f1f3dbbd0a4543271370f997c37c12aba3aca7
Chris Lewis <me@chrislewis.au>
2026-07-11 02:50:19 -0400

    Rename controller pak score save flow

--- include/controller_pak_score_delete_flow.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROLLER_PAK_SCORE_DELETE_FLOW_H
-#define CONTROLLER_PAK_SCORE_DELETE_FLOW_H
-
-void initControllerPakScoreDeleteFlow(void);
-void updateControllerPakScoreDeleteFlow(void);
-void fadeOutControllerPakScoreDeleteFlow(void);
-void updateControllerPakScoreDeleteConfirm(void);
-
-#endif
