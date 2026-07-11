6dda000115e861965169eb67c6ed2872d75e5760
Chris Lewis <me@chrislewis.au>
2026-07-11 11:14:32 -0400

    Rename ending credits sequence flow

---
+++ include/ending_credits_sequence_flow.h
@@ -0,0 +1,11 @@
+#ifndef ENDING_CREDITS_SEQUENCE_FLOW_H
+#define ENDING_CREDITS_SEQUENCE_FLOW_H
+
+#include "common.h"
+
+void initEndingCreditsSequenceFlow(void);
+void fadeInEndingCreditsSequenceFlow(void);
+void updateEndingCreditsSequenceFlow(void);
+void fadeOutEndingCreditsSequenceFlow(void);
+
+#endif
