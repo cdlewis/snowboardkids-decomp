f07a3118adb4e1e5e7f54a2fc4aa1e9df919e18f
Chris Lewis <me@chrislewis.au>
2026-07-11 04:50:36 -0400

    Rename ending object UI symbols

---
+++ include/ending_object_ui.h
@@ -0,0 +1,12 @@
+#ifndef ENDING_OBJECT_UI_H
+#define ENDING_OBJECT_UI_H
+
+#include "common.h"
+
+typedef struct EndingObjectSequenceTask EndingObjectSequenceTask;
+typedef struct EndingObjectDebugViewerTask EndingObjectDebugViewerTask;
+
+void initEndingObjectSequenceTask(EndingObjectSequenceTask *arg0);
+void initEndingObjectDebugViewerTask(EndingObjectDebugViewerTask *arg0);
+
+#endif
