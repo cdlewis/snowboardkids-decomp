fab6f598614309377feb14db532d535206f484d1
Chris Lewis <me@chrislewis.au>
2026-07-11 08:56:38 -0400

    Rename main menu race mode flow symbols

---
+++ include/main_menu_race_mode_flow.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_MENU_RACE_MODE_FLOW_H
+#define MAIN_MENU_RACE_MODE_FLOW_H
+
+#include "common.h"
+
+void startMainMenuModePreviewFlow(void);
+void initMainMenuModePreviewMenu(void);
+void fadeInMainMenuModePreviewMenu(void);
+void updateMainMenuModePreviewMenu(void);
+void fadeOutMainMenuModePreviewMenu(void);
+void exitMainMenuModePreviewMenu(void);
+void initMainMenuModePreviewRace(void);
+void fadeInMainMenuModePreviewRace(void);
+void waitForMainMenuModePreviewRaceSelection(void);
+void zoomMainMenuModePreviewRaceViewport(void);
+void runMainMenuModePreviewRace(void);
+void fadeOutMainMenuModePreviewRace(void);
+void returnToMainMenuModePreviewMenu(void);
+void startTrainingCourseFlow(void);
+void deferTrainingCourseInit(void);
+void initTrainingCourseRace(void);
+void fadeInTrainingCourseRace(void);
+void waitForTrainingCourseStartSelection(void);
+void zoomTrainingCourseRaceViewport(void);
+void runTrainingCourseUntilActionPrompt(void);
+void fadeInTrainingCourseActionMenu(void);
+void waitForTrainingCourseActionMenuSelection(void);
+void fadeOutTrainingCourseActionMenu(void);
+void handleTrainingCourseActionMenuSelection(void);
+void finishTrainingCourse(void);
+void fadeInTrainingCourseEndingDialog(void);
+void waitForTrainingCourseEndingDialog(void);
+void fadeOutTrainingCourseEndingDialog(void);
+void exitTrainingCourseEndingDialog(void);
+void returnToMainMenuFromTrainingCourse(void);
+
+#endif
