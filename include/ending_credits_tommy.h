8d31d7d4883db8117faeed25e18636a9cc9a1535
Chris Lewis <me@chrislewis.au>
2026-07-11 07:06:56 -0400

    Rename ending credits Tommy actor

---
+++ include/ending_credits_tommy.h
@@ -0,0 +1,76 @@
+#ifndef ENDING_CREDITS_TOMMY_H
+#define ENDING_CREDITS_TOMMY_H
+
+#include "common.h"
+
+typedef struct EndingCreditsTommy EndingCreditsTommy;
+
+void updateEndingTommyFinalPose(EndingCreditsTommy *arg0);
+void updateEndingTommyStartFinalPose(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase40(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitBeforePhase40(EndingCreditsTommy *arg0);
+void updateEndingTommyPhase3FAnim3(EndingCreditsTommy *arg0);
+void updateEndingTommyPhase3FAnim2(EndingCreditsTommy *arg0);
+void updateEndingTommyPhase3FAnim1(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase3E(EndingCreditsTommy *arg0);
+void updateEndingTommySetPhase3D(EndingCreditsTommy *arg0);
+void updateEndingTommyPhase3DPrep(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitBeforePhase3D(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitBeforeIdleAnim2(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitBeforeIdleAnim1(EndingCreditsTommy *arg0);
+void updateEndingTommyHopRightToIdle(EndingCreditsTommy *arg0);
+void updateEndingTommySlideLeftSetPhase3B(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase3A(EndingCreditsTommy *arg0);
+void updateEndingTommySlideRightToCenter(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase38(EndingCreditsTommy *arg0);
+void updateEndingTommySlideLeftFromFarRight(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase33Aura(EndingCreditsTommy *arg0);
+void updateEndingTommyDashOffLeftSetPhase31(EndingCreditsTommy *arg0);
+void updateEndingTommyVanishRetreat(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase2F(EndingCreditsTommy *arg0);
+void updateEndingTommyDriftAfterPhase2D(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitThenSetPhase2C(EndingCreditsTommy *arg0);
+void updateEndingTommyRunLeftThenPhase2C(EndingCreditsTommy *arg0);
+void updateEndingTommySlideFarLeft(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase2A(EndingCreditsTommy *arg0);
+void updateEndingTommySlideRightToPose(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase28(EndingCreditsTommy *arg0);
+void updateEndingTommySlideRightToMarker(EndingCreditsTommy *arg0);
+void waitEndingTommyBeforeMarkerSlide(EndingCreditsTommy *arg0);
+void updateEndingTommySlideLeftToMarker(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitThenSetPhase27(EndingCreditsTommy *arg0);
+void updateEndingTommySlideRightToPhase27Start(EndingCreditsTommy *arg0);
+void startEndingTommyPhase26Slide(EndingCreditsTommy *arg0);
+void updateEndingTommySlideLeftSetPhase25(EndingCreditsTommy *arg0);
+void waitEndingTommyBeforeDiagonalSlide(EndingCreditsTommy *arg0);
+void updateEndingTommyDiagonalSlideSetPhase22(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase21Aura(EndingCreditsTommy *arg0);
+void updateEndingTommyExitLeftAfterPhase21(EndingCreditsTommy *arg0);
+void updateEndingTommyAfterVanishWait(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitBeforeVanishReturn(EndingCreditsTommy *arg0);
+void startEndingTommyVanishReturn(EndingCreditsTommy *arg0);
+void updateEndingTommySlideLeftThenVanish(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase1E(EndingCreditsTommy *arg0);
+void updateEndingTommyExitLeftSetPhase1D(EndingCreditsTommy *arg0);
+void updateEndingTommyRepeatAnimThenVanish(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase1C(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitBeforePhase1C(EndingCreditsTommy *arg0);
+void updateEndingTommyStepBackAfterHandshake(EndingCreditsTommy *arg0);
+void waitEndingTommyHandshakeExit(EndingCreditsTommy *arg0);
+void updateEndingTommyHandshakeLoopThird(EndingCreditsTommy *arg0);
+void updateEndingTommyHandshakeLoopSecond(EndingCreditsTommy *arg0);
+void updateEndingTommyHandshakeLoopFirst(EndingCreditsTommy *arg0);
+void startEndingTommyHandshakeLoop(EndingCreditsTommy *arg0);
+void updateEndingTommySlideRightUntilPhase18(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase17(EndingCreditsTommy *arg0);
+void updateEndingTommySlideLeftToIdle(EndingCreditsTommy *arg0);
+void startEndingTommyIdleAfterAnim(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitBeforeIdleAnimB(EndingCreditsTommy *arg0);
+void updateEndingTommyWaitBeforeIdleAnimA(EndingCreditsTommy *arg0);
+void updateEndingTommyHopRightToPose(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase15(EndingCreditsTommy *arg0);
+void updateEndingTommySlideLeftToPhase15Wait(EndingCreditsTommy *arg0);
+void waitEndingTommyPhase13(EndingCreditsTommy *arg0);
+void initEndingCreditsTommy(EndingCreditsTommy *arg0);
+
+#endif
