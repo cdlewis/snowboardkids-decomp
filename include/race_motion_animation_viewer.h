1199e07eae0250003d59a20d15a158b18363e6d8
Chris Lewis <me@chrislewis.au>
2026-07-11 19:57:20 -0400

    Rename race motion animation viewer

---
+++ include/race_motion_animation_viewer.h
@@ -0,0 +1,10 @@
+#ifndef RACE_MOTION_ANIMATION_VIEWER_H
+#define RACE_MOTION_ANIMATION_VIEWER_H
+
+#include "common.h"
+
+void initRaceMotionAnimationViewer(void);
+void drawRaceMotionAnimationViewerMotionNumber(s32 arg0);
+void updateRaceMotionAnimationViewer(void);
+
+#endif
