5934e7a99f539a76fd81bc696f41e7e8a24f5b9d
Chris Lewis <me@chrislewis.au>
2026-07-11 10:56:03 -0400

    Rename ending credits effects module

---
+++ include/ending_credits_effects.h
@@ -0,0 +1,23 @@
+#ifndef ENDING_CREDITS_EFFECTS_H
+#define ENDING_CREDITS_EFFECTS_H
+
+#include "common.h"
+
+typedef struct EndingCreditsEffectActor EndingCreditsEffectActor;
+
+void spawnEndingCreditsCharacterVanishPoof(s16 arg0, s16 arg1, u8 arg2, u8 arg3);
+void initEndingCreditsNancySpeedLines(EndingCreditsEffectActor *arg0);
+void spawnEndingCreditsCharacterLoopingSparkle(s16 arg0, s16 arg1, u8 arg2);
+void initEndingCreditsTommyBigBurst(EndingCreditsEffectActor *arg0);
+void initEndingCreditsSlashRisingStar(EndingCreditsEffectActor *arg0);
+void initEndingCreditsNancyRunDust(EndingCreditsEffectActor *arg0);
+void spawnEndingCreditsSmallBurst(s16 arg0, s16 arg1);
+void spawnEndingCreditsCharacterAura(s16 arg0, s16 arg1, u8 arg2, u8 arg3);
+void spawnEndingCreditsPhaseAdvanceSparkle(s16 arg0, s16 arg1);
+void spawnEndingCreditsDelayedSparkle(s16 arg0, s16 arg1, u8 arg2);
+void initEndingCreditsTommySnowmanEntrance(EndingCreditsEffectActor *arg0);
+void initEndingCreditsTransitionSnowboardIcon(EndingCreditsEffectActor *arg0);
+void initEndingCreditsTransitionSnowflakeIcon(EndingCreditsEffectActor *arg0);
+void initEndingCreditsTransitionLogoWipe(EndingCreditsEffectActor *arg0);
+
+#endif
