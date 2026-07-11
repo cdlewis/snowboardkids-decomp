62bdba7fa53867d71a76f1eb1742ce456227a8a0
Chris Lewis <me@chrislewis.au>
2026-07-11 18:18:07 -0400

    Rename race player item effect handlers

---
+++ include/race_player_item_effects.h
@@ -0,0 +1,9 @@
+#ifndef RACE_PLAYER_ITEM_EFFECTS_H
+#define RACE_PLAYER_ITEM_EFFECTS_H
+
+#include "race_player_input.h"
+
+void updatePlayerItemEffectUse(RaceInputPlayer *player);
+void updatePlayerActionEffectUse(RaceInputPlayer *player);
+
+#endif
