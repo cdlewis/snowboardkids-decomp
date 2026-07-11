f26aea3e562a71fb10ed1df226738f95b12d895c
Chris Lewis <me@chrislewis.au>
2026-07-11 05:40:51 -0400

    Rename ending credits UI symbols

---
+++ include/ending_credits_ui.h
@@ -0,0 +1,10 @@
+#ifndef ENDING_CREDITS_UI_H
+#define ENDING_CREDITS_UI_H
+
+#include "common.h"
+
+typedef struct EndingCreditsPageTextActor EndingCreditsPageTextActor;
+
+void initEndingCreditsPageTextActor(EndingCreditsPageTextActor *arg0);
+
+#endif
