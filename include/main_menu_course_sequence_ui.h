751a504c1ec4e64046bbf214d4534db8059c1db3
Chris Lewis <me@chrislewis.au>
2026-07-11 06:00:18 -0400

    Rename main menu course sequence UI

---
+++ include/main_menu_course_sequence_ui.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_MENU_COURSE_SEQUENCE_UI_H
+#define MAIN_MENU_COURSE_SEQUENCE_UI_H
+
+#include "common.h"
+
+typedef struct MainMenuCourseSequenceActor MainMenuCourseSequenceActor;
+
+void initMainMenuCourseSequenceResultOptions(MainMenuCourseSequenceActor *arg0);
+void initMainMenuCourseSequenceIntroMessage(MainMenuCourseSequenceActor *arg0);
+void initMainMenuCourseSequenceFinalMessage(MainMenuCourseSequenceActor *arg0);
+void initMainMenuCourseSequenceTitlePanel(MainMenuCourseSequenceActor *arg0);
+
+#endif
