43c84b0b952f079d6d0a6cc024e1028ff9ed1c2a
Chris Lewis <me@chrislewis.au>
2026-07-11 17:32:36 -0400

    Rename race flow callbacks

--- include/shop_menu_flow.h
+++ include/shop_menu_flow.h
@@ -1,8 +1,8 @@
 #ifndef SHOP_MENU_FLOW_H
 #define SHOP_MENU_FLOW_H
 
-void func_8000C280(void);
-void func_8000C3C8(void);
-void func_8000C52C(void);
+void initControllerPakContinuePromptFlow(void);
+void updateControllerPakContinuePromptFlow(void);
+void closeControllerPakContinuePromptFlow(void);
 
 #endif
