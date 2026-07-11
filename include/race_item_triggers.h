49485a4a7cfcd818aa23777c557ce1e975bf621a
Chris Lewis <me@chrislewis.au>
2026-07-10 22:54:21 -0400

    Declare race item trigger functions

---
+++ include/race_item_triggers.h
@@ -0,0 +1,9 @@
+#ifndef RACE_ITEM_TRIGGERS_H
+#define RACE_ITEM_TRIGGERS_H
+
+#include "race_input_history.h"
+
+void func_800849E0(RaceInputPlayer *player);
+void func_80084D74(RaceInputPlayer *player);
+
+#endif
