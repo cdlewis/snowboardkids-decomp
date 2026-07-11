b060ae09a5d1b2dd3d4d1dcdd7ce41351265926a
Chris Lewis <me@chrislewis.au>
2026-07-11 01:58:11 -0400

    Rename pre-race Controller Pak warning flow

--- src/controller_pak_pre_race_warning_flow.c
+++ src/controller_pak_pre_race_warning_flow.c
@@ -2,7 +2,7 @@
 #include "effect_task_scheduler.h"
 #include "asset_manager.h"
 #include "character_select_course_menu.h"
-#include "controller_pak_race_warning_flow.h"
+#include "controller_pak_pre_race_warning_flow.h"
 #include "controller_pak_ui.h"
 #include "input_task_scheduler.h"
 #include "menu_rendering.h"
@@ -19,7 +19,7 @@ extern u8 D_5E26E0[];
 extern u8 D_5E34A0[];
 extern u8 D_5E0E40[];
 
-void initControllerPakRaceWarningFlow(void) {
+void initControllerPakPreRaceWarningFlow(void) {
     func_800704F0();
     func_8007066C(0, 0xA0, 0x78, 0x120, 0xD0, 0x140, 0xF0, 1.333333373f);
     D_800DEED4 = 0;
@@ -29,11 +29,11 @@ void initControllerPakRaceWarningFlow(void) {
     loadCompressedRomAsset(D_5E26E0, D_5E34A0, 0x26);
     func_80070EC0(0);
     func_80071408(initControllerPakMessageIcon, 0, 0x5E);
-    func_8009956C(updateControllerPakRaceWarningFirstMessage, 0);
+    func_8009956C(fadeInControllerPakPreRaceWarningFirstMessage, 0);
     func_8007105C();
 }
 
-void updateControllerPakRaceWarningFirstMessage(void) {
+void fadeInControllerPakPreRaceWarningFirstMessage(void) {
     if (D_801235B8->fade != 0) {
         D_801235B8->fade = func_80013F88(D_801235B8->fade, 0xF, 0);
     } else {
@@ -41,12 +41,12 @@ void updateControllerPakRaceWarningFirstMessage(void) {
     }
     if (D_801235B8->timer >= 0x50) {
         D_801235B8->timer = 0;
-        func_8009956C(fadeOutControllerPakRaceWarningFirstMessage, 0);
+        func_8009956C(fadeOutControllerPakPreRaceWarningFirstMessage, 0);
     }
     func_8007105C();
 }
 
-void fadeOutControllerPakRaceWarningFirstMessage(void) {
+void fadeOutControllerPakPreRaceWarningFirstMessage(void) {
     if (D_801235B8->fade != 0xFF) {
         D_801235B8->fade = func_80013F88(D_801235B8->fade, 0xF, 1);
         if (D_801235B8->fade == 0xFF) {
@@ -54,35 +54,35 @@ void fadeOutControllerPakRaceWarningFirstMessage(void) {
             loadCompressedRomAsset(D_5E0E40, D_5E26E0, 0x26);
         }
     } else {
-        func_8009956C(waitControllerPakRaceWarningSecondMessage, 0);
+        func_8009956C(waitControllerPakPreRaceWarningSecondMessage, 0);
     }
     func_8007105C();
 }
 
-void waitControllerPakRaceWarningSecondMessage(void) {
+void waitControllerPakPreRaceWarningSecondMessage(void) {
     D_801235B8->timer++;
     if (D_801235B8->timer >= 0x14) {
         D_801235B8->fade = 0xFF;
         D_800DEF14 = D_801235B8->fade;
         D_801235B8->timer = 0;
-        func_8009956C(updateControllerPakRaceWarningSecondMessage, 0);
+        func_8009956C(fadeInControllerPakPreRaceWarningSecondMessage, 0);
     }
     func_8007105C();
 }
 
-void updateControllerPakRaceWarningSecondMessage(void) {
+void fadeInControllerPakPreRaceWarningSecondMessage(void) {
     if (D_801235B8->fade != 0) {
         D_801235B8->fade = func_80013F88(D_801235B8->fade, 0xF, 0);
     } else {
         D_801235B8->timer++;
     }
     if (D_801235B8->timer >= 0x50) {
-        func_8009956C(fadeOutControllerPakRaceWarningFlow, 0);
+        func_8009956C(fadeOutControllerPakPreRaceWarningFlow, 0);
     }
     func_8007105C();
 }
 
-void fadeOutControllerPakRaceWarningFlow(void) {
+void fadeOutControllerPakPreRaceWarningFlow(void) {
     if (D_801235B8->fade != 0xFF) {
         D_801235B8->fade = func_80013F88(D_801235B8->fade, 0xF, 1);
         if (D_801235B8->fade == 0xFF) {
