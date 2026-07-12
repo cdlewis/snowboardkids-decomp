97d00355ca6bb54a60b4570feaacc99da33d09f8
Chris Lewis <me@chrislewis.au>
2026-07-11 20:50:56 -0400

    Rename race motion test viewer symbols

---
+++ include/race_motion_test_viewer.h
@@ -0,0 +1,10 @@
+#ifndef RACE_MOTION_TEST_VIEWER_H
+#define RACE_MOTION_TEST_VIEWER_H
+
+#include "common.h"
+
+void initRaceMotionTestViewer(void);
+void drawRaceMotionTestViewerMotionNumber(s32 unused);
+void updateRaceMotionTestViewer(void);
+
+#endif
