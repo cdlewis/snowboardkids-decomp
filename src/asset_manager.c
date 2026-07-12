d6d7d7217bd3dff3017c1413315a8c0d4bf318ba
Chris Lewis <me@chrislewis.au>
2026-07-12 13:25:47 -0400

    Improve decompressHuffmanAssetPayload nonmatching attempt

--- src/asset_manager.c
+++ src/asset_manager.c
@@ -159,7 +159,7 @@ void removeHuffmanQueueNode(s16 arg0) {
     }
 }
 
-// decompressHuffmanAssetPayload best match: 62.649%
+// decompressHuffmanAssetPayload best match: 73.790% (nonmatchings/decompressHuffmanAssetPayload-8331816093655448999/base_15.c)
 #pragma GLOBAL_ASM("asm/nonmatchings/asset_manager/decompressHuffmanAssetPayload.s")
 
 #ifdef NON_MATCHING
@@ -181,7 +181,6 @@ void decompressHuffmanAssetPayload(u8 arg0, s32 arg1, s32 arg2, s32 arg3) {
     s32 temp_a2;
     s32 temp_a2_2;
     s32 temp_at;
-    s32 temp_s2;
     s32 var_a0;
     u8 *var_a1;
     u8 *var_s1;
@@ -191,11 +190,11 @@ void decompressHuffmanAssetPayload(u8 arg0, s32 arg1, s32 arg2, s32 arg3) {
     s8 temp_t3;
     u8 *var_s1_2;
     u8 *var_v1;
-    u8 temp_s3;
-    u8 temp_s6;
+    s32 temp_s3;
+    s32 temp_s6;
     u8 temp_t7;
     u8 temp_t8;
-    u8 var_s0;
+    s32 var_s0;
     u8 *var_a1_2;
 
     gHuffmanQueueHead = -1;
@@ -206,35 +205,36 @@ void decompressHuffmanAssetPayload(u8 arg0, s32 arg1, s32 arg2, s32 arg3) {
     var_s1 = (u8 *) arg1;
 loop_1:
     temp_s3 = *var_s1;
-    temp_s2 = var_s2 + 1;
+    var_s2 += 1;
     var_s1_2 = var_s1 + 1;
-    if (temp_s2 == 1) {
-block_3:
-        temp_s6 = var_s1[1];
-        var_s2 = temp_s2 + 1;
-        temp_at = (s32) temp_s6 < (s32) temp_s3;
-        var_s1 = var_s1_2 + 1;
-        var_s0 = temp_s3;
-        if (temp_at == 0) {
-            do {
-                temp_t7 = *var_s1;
-                var_s2 += 1;
-                var_s1 += 1;
-                temp_v0 = &gHuffmanNodes[gHuffmanNodeCount];
-                temp_v0->left = -1;
-                temp_v0->right = -1;
-                temp_v0->value = (s16) var_s0;
-                temp_v0->weight = (s16) temp_t7;
-                insertHuffmanQueueNode((s16) gHuffmanNodeCount);
-                var_s0 += 1;
-                gHuffmanNodeCount += 1;
-            } while ((temp_s6 + 1) != var_s0);
+    if (var_s2 != 1) {
+        if (temp_s3 == 0) {
+            goto loop_7;
         }
-        goto loop_1;
     }
-    if (temp_s3 != 0) {
-        goto block_3;
+block_3:
+    temp_s6 = var_s1[1];
+    var_s2 += 1;
+    temp_at = (s32) temp_s6 < (s32) temp_s3;
+    var_s1 = var_s1_2 + 1;
+    var_s0 = temp_s3;
+    if (temp_at == 0) {
+        temp_s3 = temp_s6 + 1;
+        do {
+            temp_t7 = *var_s1;
+            var_s2 += 1;
+            var_s1 += 1;
+            temp_v0 = &gHuffmanNodes[gHuffmanNodeCount];
+            temp_v0->left = -1;
+            temp_v0->right = -1;
+            temp_v0->value = (s16) var_s0;
+            temp_v0->weight = (s16) temp_t7;
+            insertHuffmanQueueNode((s16) gHuffmanNodeCount);
+            var_s0 += 1;
+            gHuffmanNodeCount += 1;
+        } while (temp_s3 != var_s0);
     }
+    goto loop_1;
 loop_7:
     if (gHuffmanQueueCount >= 2) {
         temp_s3_2 = gHuffmanQueueTail;
