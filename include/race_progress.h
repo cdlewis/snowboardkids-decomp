3a88d488a271e1f58403064e684430c678cc251a
Chris Lewis <me@chrislewis.au>
2026-07-11 21:37:26 -0400

    Rename race progress tracker symbols

--- include/race_progress.h
+++ include/race_progress.h
@@ -1,14 +1,12 @@
-#ifndef RACE_POSITION_TRACKER_H
-#define RACE_POSITION_TRACKER_H
+#ifndef RACE_PROGRESS_H
+#define RACE_PROGRESS_H
 
 #include "common.h"
 
-struct RacePositionPlayer;
-
-#define updateRacePositionTracker func_8007B250
+struct RaceProgressPlayer;
 
 void updateRacePositionTracker(void);
-void func_8007BB08(struct RacePositionPlayer *player);
+void updateRacePlayerCheckpointEvent(struct RaceProgressPlayer *player);
 s32 getSmoothedRacePlayerPathOffset(s32 playerIndex, s32 pathIndex, s32 rankSlot);
 s32 getRacePlayerPathOffset(s32 playerIndex, s32 pathIndex);
 
