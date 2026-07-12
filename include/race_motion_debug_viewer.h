f5f41dfc19beb7daca6b810f0515955eb753cee5
Chris Lewis <me@chrislewis.au>
2026-07-11 20:39:30 -0400

    Rename race motion debug viewer

---
+++ include/race_motion_debug_viewer.h
@@ -0,0 +1,10 @@
+#ifndef RACE_MOTION_DEBUG_VIEWER_H
+#define RACE_MOTION_DEBUG_VIEWER_H
+
+#include "common.h"
+
+void initRaceMotionDebugViewer(void);
+void drawRaceMotionDebugViewerMotionNumber(s32 unused);
+void updateRaceMotionDebugViewer(void);
+
+#endif
