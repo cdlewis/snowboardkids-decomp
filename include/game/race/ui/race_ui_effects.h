9201bd63ac8d6aa7ee69ed7a2d6888af9d7fcd08
Chris Lewis <me@chrislewis.au>
2026-07-13 19:41:19 -0400

    Clean up hit prize total money reveal

--- include/game/race/ui/race_ui_effects.h
+++ include/game/race/ui/race_ui_effects.h
@@ -106,7 +106,7 @@ void func_8005AFEC(void *arg0);
 void func_8005B068(void *arg0);
 void func_8005B14C(struct RaceUiCounterActor *arg0);
 void func_8005B344(void *arg0);
-void func_8005B3EC(void *arg0);
+void updateRaceUiHitPrizeRevealTotalMoney(struct RaceUiCounterActor *arg0);
 void func_8005B49C(void *arg0);
 void func_8005B55C(void *arg0);
 void func_8005B61C(void *arg0);
