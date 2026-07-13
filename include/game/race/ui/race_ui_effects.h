da01519afcc8f444a4bc43831a00aba1fc0e1b75
Chris Lewis <me@chrislewis.au>
2026-07-13 19:57:44 -0400

    Clean up trick prize complete bonus reveal

--- include/game/race/ui/race_ui_effects.h
+++ include/game/race/ui/race_ui_effects.h
@@ -145,7 +145,7 @@ void func_8005DD88(void *arg0);
 void func_8005DE6C(struct RaceUiCourseStatsActor *arg0);
 void func_8005E064(void *arg0);
 void updateRaceUiTrickPrizePayoutRevealMoneyRow(struct RaceUiCourseStatsActor *arg0);
-void func_8005E1BC(void *arg0);
+void updateRaceUiTrickPrizePayoutRevealCompleteBonus(struct RaceUiCourseStatsActor *arg0);
 void func_8005E27C(void *arg0);
 void func_8005E33C(void *arg0);
 void func_8005E3F8(void *arg0);
