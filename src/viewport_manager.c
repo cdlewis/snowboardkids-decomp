5384a50ea3ed4de9237e16b85050976618e50629
Chris Lewis <me@chrislewis.au>
2026-07-13 05:27:25 -0400

    Improve configureViewport partial match

--- src/viewport_manager.c
+++ src/viewport_manager.c
@@ -86,7 +86,7 @@ void resetViewport(s32 arg0) {
     viewport->unk1E = 0;
 }
 
-// configureViewport best match: 90.016% (nonmatchings/configureViewport-3357475854818838508/base_1.c)
+// configureViewport best match: 97.621% (nonmatchings/configureViewport-2870645799593382959/base_18.c)
 
 #pragma GLOBAL_ASM("asm/nonmatchings/viewport_manager/configureViewport.s")
 
@@ -97,59 +97,60 @@ extern void guPerspective(ViewportMtx *, u16 *, f32, f32, f32, f32, f32);
 void configureViewport(s32 arg0, s32 arg1, s32 arg2, u16 arg3, u16 arg4, u16 arg5, u16 arg6, f32 arg7) {
     ViewportState *viewport;
     s32 halfHeight;
-    s32 halfWidth;
+    s32 boundsInvalid;
+    u32 boundsValid;
     f32 fovy;
-    u16 *arg3Ptr;
+    s32 halfWidth;
 
-    viewport = &gViewportStates[arg0];
-    viewport->viewportTranslateX = arg1 * 4;
-    viewport->active = 1;
-    viewport->viewportTranslateY = arg2 * 4;
-    viewport->viewportScaleX = arg5 * 2;
-    viewport->viewportScaleY = arg6 * 2;
-
-    arg3Ptr = &arg3;
-    arg3 = *arg3Ptr & 0xFFFF;
+    boundsValid = (halfHeight = 1);
+    (&gViewportStates[arg0])->viewportTranslateX = arg1 * 4;
+    (&gViewportStates[arg0])->active = halfHeight;
+    (&gViewportStates[arg0])->viewportTranslateY = arg2 * 4;
+    (&gViewportStates[arg0])->viewportScaleX = arg5 * 2;
+    (&gViewportStates[arg0])->viewportScaleY = arg6 * 2;
     halfWidth = arg3 / 2;
-    viewport->right = halfWidth + arg1;
-    viewport->left = arg1 - halfWidth;
+    (&gViewportStates[arg0])->left = arg1 - halfWidth;
+    (&gViewportStates[arg0])->right = halfWidth + arg1;
     halfHeight = arg4 / 2;
-    viewport->top = arg2 - halfHeight;
-    viewport->bottom = halfHeight + arg2;
-    viewport->right = viewport->right;
-    viewport->screenBoundsValid = 1;
-    viewport->left = viewport->left;
-    viewport->top = viewport->top;
-    viewport->bottom = viewport->bottom;
+    (&gViewportStates[arg0])->top = arg2 - halfHeight;
+    (&gViewportStates[arg0])->bottom = halfHeight + arg2;
+    (&gViewportStates[arg0])->left = (&gViewportStates[arg0])->left;
+    (&gViewportStates[arg0])->top = (&gViewportStates[arg0])->top;
+    (&gViewportStates[arg0])->right = (&gViewportStates[arg0])->right;
+    (&gViewportStates[arg0])->bottom = (&gViewportStates[arg0])->bottom;
+    (&gViewportStates[arg0])->screenBoundsValid = boundsValid;
+    boundsInvalid = 0;
 
-    if (viewport->right < 0) {
-        viewport->screenBoundsValid = 0;
+    if ((&gViewportStates[arg0])->right < 0) {
+        (&gViewportStates[arg0])->screenBoundsValid = 0;
     }
-    if (viewport->bottom < 0) {
-        viewport->screenBoundsValid = 0;
+    if ((&gViewportStates[arg0])->bottom < 0) {
+        (&gViewportStates[arg0])->screenBoundsValid = boundsInvalid;
     }
-    if (viewport->left >= 0x140) {
-        viewport->screenBoundsValid = 0;
+    if ((&gViewportStates[arg0])->left >= 0x140) {
+        (&gViewportStates[arg0])->screenBoundsValid = 0;
     }
-    if (viewport->top >= 0xF0) {
-        viewport->screenBoundsValid = 0;
+    if ((&gViewportStates[arg0])->top >= 0xF0) {
+        (&gViewportStates[arg0])->screenBoundsValid = 0;
     }
-    if (viewport->left < 0) {
-        viewport->left = 0;
+    if ((&gViewportStates[arg0])->left < 0) {
+        (&gViewportStates[arg0])->left = 0;
     }
-    if (viewport->top < 0) {
-        viewport->top = 0;
+    if ((&gViewportStates[arg0])->top < 0) {
+        (&gViewportStates[arg0])->top = 0;
     }
-    if (viewport->right >= 0x140) {
-        viewport->right = 0x13F;
+    if ((&gViewportStates[arg0])->right >= 0x140) {
+        (&gViewportStates[arg0])->right = 0x13F;
     }
-    if (viewport->bottom >= 0xF0) {
-        viewport->bottom = 0xEF;
+    if ((&gViewportStates[arg0])->bottom >= 0xF0) {
+        (&gViewportStates[arg0])->bottom = 0xEF;
     }
 
     fovy = 70.0f;
-    guPerspective(&viewport->projection, &viewport->perspectiveNorm, fovy, arg7, 10.0f, 2800.0f, 0.5f);
-    guPerspective(&viewport->overlayProjection, &viewport->overlayPerspectiveNorm, fovy, arg7, 10.0f, gDefaultViewportOverlayFarClip, 0.5f);
+    guPerspective(&(&gViewportStates[arg0])->projection, &(&gViewportStates[arg0])->perspectiveNorm, fovy, arg7, 10.0f,
+                  2800.0f, 0.5f);
+    guPerspective(&(&gViewportStates[arg0])->overlayProjection, &(&gViewportStates[arg0])->overlayPerspectiveNorm, fovy,
+                  arg7, 10.0f, gDefaultViewportOverlayFarClip, 0.5f);
 }
 #endif
 
