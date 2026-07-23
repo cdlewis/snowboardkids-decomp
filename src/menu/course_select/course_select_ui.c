9ac38588514cea7e669c1315afe977c75bfa7a9d
Chris Lewis <me@chrislewis.au>
2026-07-23 03:03:02 -0400

    Improve initCourseSelectCourseIconList match

--- src/menu/course_select/course_select_ui.c
+++ src/menu/course_select/course_select_ui.c
@@ -1057,7 +1057,7 @@ void updateCourseSelectCourseIconList(CourseSelectIconListActor *arg0) {
 }
 #endif
 
-// initCourseSelectCourseIconList best match: 96.310% (nonmatchings/initCourseSelectCourseIconList-6934502587000073416/base_19.c)
+// initCourseSelectCourseIconList best match: 97.690% (nonmatchings/initCourseSelectCourseIconList-3379532139742180785/base_4.c)
 #pragma GLOBAL_ASM("asm/nonmatchings/menu/course_select/course_select_ui/initCourseSelectCourseIconList.s")
 
 #ifdef NON_MATCHING
@@ -1079,54 +1079,7 @@ void initCourseSelectCourseIconList(CourseSelectIconListActor *arg0) {
         layoutIndex = 2;
     }
 
-    playerIndex = 0;
-    if ((s32)gPlayerCount > 0) {
-        courseUnlocked = D_8010AEA0;
-        yLayout = gCourseSelectIconListYLayout[layoutIndex];
-        xLayout = gCourseSelectIconListXLayout[layoutIndex];
-        do {
-            if (*courseUnlocked != 0) {
-                isCharacterFive = gRacePlayers[playerIndex].selectedCharacterId == 5;
-                hasExtraCourse = 1;
-                if (isCharacterFive) {
-                    hasExtraCourse = 0;
-                }
-            } else {
-                hasExtraCourse = 0;
-            }
-
-            actor->speed[playerIndex] = yLayout[hasExtraCourse];
-            actor->baseX[playerIndex] = yLayout[((playerIndex & 1) * 2) + hasExtraCourse + 2];
-            actor->targetX[playerIndex] = yLayout[((playerIndex & 1) * 2) + 2];
-            actor->targetY[playerIndex] = xLayout[((playerIndex >= 2) * 2) + 1];
-            actor->startY[playerIndex] = xLayout[(playerIndex >= 2) * 2];
-
-            actor->clipTop = 0x78;
-            actor->clipRight = 0xA0;
-            actor->clipBottom = 0x78;
-            if (gPlayerCount == 1) {
-                actor->clipLeft = 0xA0;
-            } else if (gPlayerCount == 2) {
-                actor->clipLeft = 0x7E;
-            } else {
-                actor->clipLeft = 0x88;
-            }
-
-            iconIndex = 0;
-            do {
-                actor->y[playerIndex][iconIndex] = actor->startY[playerIndex];
-                actor->x[playerIndex][iconIndex] =
-                    actor->baseX[playerIndex] + (actor->speed[playerIndex] * iconIndex);
-                iconIndex++;
-            } while (iconIndex < 5);
-            actor->timer[playerIndex] = 0;
-            actor->itemCounts[playerIndex] = 1;
-            actor->state[playerIndex] = 0;
-
-            courseUnlocked++;
-            playerIndex++;
-        } while (playerIndex < (s32)gPlayerCount);
-    }
+    do { playerIndex = 0; if ((s32)gPlayerCount > 0) { courseUnlocked = D_8010AEA0; yLayout = gCourseSelectIconListYLayout[layoutIndex]; xLayout = gCourseSelectIconListXLayout[layoutIndex]; do { if (*courseUnlocked != 0) { isCharacterFive = gRacePlayers[playerIndex].selectedCharacterId == 5; hasExtraCourse = 1; if (isCharacterFive) { hasExtraCourse = 0; } } else { hasExtraCourse = 0; } actor->speed[playerIndex] = yLayout[hasExtraCourse]; actor->baseX[playerIndex] = yLayout[((playerIndex & 1) * 2) + hasExtraCourse + 2]; actor->targetX[playerIndex] = yLayout[((playerIndex & 1) * 2) + 2]; actor->targetY[playerIndex] = xLayout[((playerIndex >= 2) * 2) + 1]; actor->startY[playerIndex] = xLayout[(playerIndex >= 2) * 2]; actor->clipBottom = 0x78; actor->clipRight = 0xA0; actor->clipTop = 0x78; if (gPlayerCount == 1) { actor->clipLeft = 0xA0; } else if (gPlayerCount == 2) { actor->clipLeft = 0x7E; } else { actor->clipLeft = 0x88; } iconIndex = 0; do { actor->y[playerIndex][iconIndex] = actor->startY[playerIndex]; actor->x[playerIndex][iconIndex] = actor->baseX[playerIndex] + (actor->speed[playerIndex] * iconIndex); iconIndex++; } while (iconIndex < 5); actor->state[playerIndex] = 0; actor->itemCounts[playerIndex] = 1; actor->timer[playerIndex] = 0; courseUnlocked++; playerIndex++; } while (playerIndex < (s32)gPlayerCount); } } while (0);
     setCallbackTaskCallback(arg0, updateCourseSelectCourseIconList);
 }
 #endif
