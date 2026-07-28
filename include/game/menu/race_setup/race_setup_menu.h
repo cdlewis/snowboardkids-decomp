1a03b5b2bddd90e00eb1776cb6bd25bad33926a7
Chris Lewis <me@chrislewis.au>
2026-07-28 19:10:19 -0400

    Consolidate race setup menu substate type

--- include/game/menu/race_setup/race_setup_menu.h
+++ include/game/menu/race_setup/race_setup_menu.h
@@ -3,6 +3,21 @@
 
 #include "common.h"
 
+typedef struct {
+    /* 0x00 */ u8 state;
+    /* 0x01 */ u8 timer;
+    /* 0x02 */ s16 alpha;
+    /* 0x04 */ u8 forceUpdate;
+    /* 0x05 */ u8 pad5;
+    /* 0x06 */ u16 pendingStatusCodes[4];
+    /* 0x0E */ u8 statusTransitionStates[4];
+    /* 0x12 */ u16 nextStatusCodes[4];
+    /* 0x1A */ u8 pad1A[0x1E];
+} RaceSetupMenuSubState;
+
+extern RaceSetupMenuSubState gRaceSetupMenuSubState;
+extern u8 gRaceSetupSavePanelFrameState;
+
 void initRaceSetupMenu(void);
 void updateRaceSetupPlayerCountMenu(void);
 void initRaceSetupSaveMenu(void);
