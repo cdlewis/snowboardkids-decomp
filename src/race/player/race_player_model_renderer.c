fbd46cd712de0e2360b536749c718b19c66e7602
Chris Lewis <me@chrislewis.au>
2026-08-03 01:44:57 -0400

    task-runner(match-data-segments): match data segment 'race/player/race_player_model_renderer'

--- src/race/player/race_player_model_renderer.c
+++ src/race/player/race_player_model_renderer.c
@@ -55,41 +55,167 @@ struct RacePlayerModelRenderState {
     /* 0x530 */ void *partVtx[RACE_PLAYER_MODEL_RENDERER_PLAYER_PART_CAPACITY];
 };
 
+extern u32 gRacePlayerShadowMatrixTemplate[];
+
+Gfx gRacePlayerShadowRenderSetupDisplayList[] = {
+    {{ 0xE7000000, 0x00000000 }},
+    {{ 0xBB000000, 0xFFFFFFFF }},
+    {{ 0xFCFFFFFF, 0xFFFE793C }},
+    {{ 0xB900031D, 0x005049D8 }},
+    {{ 0xB8000000, 0x00000000 }},
+};
+
+Gfx *gRacePlayerModelRootPartDisplayLists[RACE_PLAYER_MODEL_RENDERER_PART_COUNT + 2] = {
+    (Gfx *)0x020004B8, (Gfx *)0x02000180, (Gfx *)0x02000828,
+    (Gfx *)0x020004B8, (Gfx *)0x02000180, (Gfx *)0x02000828,
+    (Gfx *)0x020004B8, (Gfx *)0x02000180, (Gfx *)0x02000828,
+    (Gfx *)0x02002F68, (Gfx *)0x02003408, (Gfx *)0x02003768,
+    (Gfx *)0x02001AE0, (Gfx *)0x02001F80, (Gfx *)0x02002300,
+};
+
+Gfx *gRaceGhostPlayerModelRootPartDisplayLists[RACE_PLAYER_MODEL_RENDERER_PART_COUNT + 2] = {
+    (Gfx *)0x02003BC0, (Gfx *)0x02003A48, (Gfx *)0x02003DB0,
+    (Gfx *)0x02003BC0, (Gfx *)0x02003A48, (Gfx *)0x02003DB0,
+    (Gfx *)0x02003BC0, (Gfx *)0x02003A48, (Gfx *)0x02003DB0,
+    (Gfx *)0x02004720, (Gfx *)0x02004A10, (Gfx *)0x02004B70,
+    (Gfx *)0x02003F68, (Gfx *)0x02004228, (Gfx *)0x020043E8,
+};
+
+Gfx *gRacePlayerModelPart0DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000000, (Gfx *)0x02000000, (Gfx *)0x02000000,
+    (Gfx *)0x02000000, (Gfx *)0x02000000, (Gfx *)0x02000000,
+};
+
+Gfx *gRacePlayerModelPart1DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000068, (Gfx *)0x02000128, (Gfx *)0x020000E0,
+    (Gfx *)0x020001C8, (Gfx *)0x020000E0, (Gfx *)0x020002A0,
+};
+
+Gfx *gRacePlayerModelPart2DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x020000D8, (Gfx *)0x02000218, (Gfx *)0x020001D0,
+    (Gfx *)0x02000378, (Gfx *)0x020002D0, (Gfx *)0x02000330,
+};
+
+Gfx *gRacePlayerModelPart3DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000168, (Gfx *)0x02000308, (Gfx *)0x02000338,
+    (Gfx *)0x02000528, (Gfx *)0x020004E0, (Gfx *)0x02000518,
+};
+
+Gfx *gRacePlayerModelPart4DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x020001D8, (Gfx *)0x020003F8, (Gfx *)0x02000428,
+    (Gfx *)0x020006D8, (Gfx *)0x020006B0, (Gfx *)0x020005B8,
+};
+
+Gfx *gRacePlayerModelPart5DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000268, (Gfx *)0x020004F0, (Gfx *)0x020004F0,
+    (Gfx *)0x02000888, (Gfx *)0x020008C0, (Gfx *)0x020007B0,
+};
+
+Gfx *gRacePlayerModelPart6DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000540, (Gfx *)0x02000850, (Gfx *)0x020008A0,
+    (Gfx *)0x02000CD8, (Gfx *)0x02000CE0, (Gfx *)0x02000CF0,
+};
+
+Gfx *gRacePlayerModelPart7DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000A28, (Gfx *)0x02000BE8, (Gfx *)0x02000DD8,
+    (Gfx *)0x02001148, (Gfx *)0x02001298, (Gfx *)0x020010E8,
+};
+
+Gfx *gRacePlayerModelPart8DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000AA0, (Gfx *)0x02000D10, (Gfx *)0x02000F50,
+    (Gfx *)0x02001210, (Gfx *)0x02001408, (Gfx *)0x02001170,
+};
+
+Gfx *gRacePlayerModelPart9DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000C48, (Gfx *)0x02000FD0, (Gfx *)0x02001178,
+    (Gfx *)0x020014D0, (Gfx *)0x02001608, (Gfx *)0x02001410,
+};
+
+Gfx *gRacePlayerModelPart10DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000CC0, (Gfx *)0x02001128, (Gfx *)0x020012F0,
+    (Gfx *)0x02001590, (Gfx *)0x02001778, (Gfx *)0x02001498,
+};
+
+Gfx *gRacePlayerModelPart11DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02000E68, (Gfx *)0x020013E8, (Gfx *)0x02001508,
+    (Gfx *)0x02001850, (Gfx *)0x02001978, (Gfx *)0x02001738,
+};
+
+Gfx *gRacePlayerModelPart12DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x020010C0, (Gfx *)0x02001630, (Gfx *)0x02001760,
+    (Gfx *)0x02001A98, (Gfx *)0x02001BA0, (Gfx *)0x020017C8,
+};
+
+Gfx *gRaceGhostPlayerModelPart0DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02001348, (Gfx *)0x02001878, (Gfx *)0x020019B8,
+    (Gfx *)0x02001CE0, (Gfx *)0x02001E08, (Gfx *)0x02001858,
+};
+
+Gfx *gRaceGhostPlayerModelPart1DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x020013B0, (Gfx *)0x020019A0, (Gfx *)0x02001A98,
+    (Gfx *)0x02001EA8, (Gfx *)0x02001EE8, (Gfx *)0x02001AF8,
+};
+
+Gfx *gRaceGhostPlayerModelPart2DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02001420, (Gfx *)0x02001A90, (Gfx *)0x02001B88,
+    (Gfx *)0x02002058, (Gfx *)0x020020D8, (Gfx *)0x02001B88,
+};
+
+Gfx *gRaceGhostPlayerModelPart3DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x020014B0, (Gfx *)0x02001B80, (Gfx *)0x02001CF0,
+    (Gfx *)0x02002208, (Gfx *)0x020022E8, (Gfx *)0x02001D70,
+};
+
+Gfx *gRaceGhostPlayerModelPart4DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02001520, (Gfx *)0x02001C70, (Gfx *)0x02001DE0,
+    (Gfx *)0x020023B8, (Gfx *)0x020024B8, (Gfx *)0x02001E10,
+};
+
+Gfx *gRaceGhostPlayerModelPart5DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x020015B0, (Gfx *)0x02001D68, (Gfx *)0x02001EA8,
+    (Gfx *)0x02002568, (Gfx *)0x020026C8, (Gfx *)0x02002008,
+};
+
+Gfx *gRaceGhostPlayerModelPart6DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02001888, (Gfx *)0x020020C8, (Gfx *)0x02002258,
+    (Gfx *)0x020029B8, (Gfx *)0x02002AE8, (Gfx *)0x02002548,
+};
+
+Gfx *gRaceGhostPlayerModelPart7DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02001D70, (Gfx *)0x02002460, (Gfx *)0x02002790,
+    (Gfx *)0x02002E28, (Gfx *)0x020030A0, (Gfx *)0x02002940,
+};
+
+Gfx *gRaceGhostPlayerModelPart8DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02001DE8, (Gfx *)0x02002588, (Gfx *)0x02002908,
+    (Gfx *)0x02002EF0, (Gfx *)0x02003210, (Gfx *)0x020029C8,
+};
+
+Gfx *gRaceGhostPlayerModelPart9DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02001F90, (Gfx *)0x02002848, (Gfx *)0x02002B30,
+    (Gfx *)0x020031B0, (Gfx *)0x02003410, (Gfx *)0x02002C68,
+};
+
+Gfx *gRaceGhostPlayerModelPart10DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02002008, (Gfx *)0x020029A0, (Gfx *)0x02002CA8,
+    (Gfx *)0x02003270, (Gfx *)0x02003580, (Gfx *)0x02002CF0,
+};
+
+Gfx *gRaceGhostPlayerModelPart11DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x020021B0, (Gfx *)0x02002C60, (Gfx *)0x02002EC0,
+    (Gfx *)0x02003530, (Gfx *)0x02003780, (Gfx *)0x02002F90,
+};
+
+Gfx *gRaceGhostPlayerModelPart12DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS] = {
+    (Gfx *)0x02002408, (Gfx *)0x02002EA8, (Gfx *)0x02003118,
+    (Gfx *)0x02003778, (Gfx *)0x020039A8, (Gfx *)0x02003020,
+};
+
 extern void *allocMenuRenderScratch(s32 size);
 extern void *allocFixedTransformMatrix(void *source);
 
 extern s16 gUiBlinkTimer;
 extern Gfx *gRegionAllocPtr;
-extern Gfx gRacePlayerShadowRenderSetupDisplayList[];
-extern u32 gRacePlayerShadowMatrixTemplate[];
-extern u32 gRacePlayerModelRootPartDisplayLists[RACE_PLAYER_MODEL_RENDERER_PART_COUNT + 2];
-extern u32 gRaceGhostPlayerModelRootPartDisplayLists[RACE_PLAYER_MODEL_RENDERER_PART_COUNT + 2];
-extern u32 gRacePlayerModelPart0DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart1DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart2DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart3DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart4DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart5DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart6DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart7DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart8DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart9DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart10DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart11DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRacePlayerModelPart12DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart0DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart1DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart2DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart3DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart4DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart5DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart6DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart7DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart8DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart9DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart10DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart11DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
-extern u32 gRaceGhostPlayerModelPart12DisplayLists[RACE_PLAYER_MODEL_RENDERER_TEXTURE_VARIANTS];
 
 void drawRacePlayerGroundShadow(RacePlayerModelRenderState *player) {
     s32 i;
