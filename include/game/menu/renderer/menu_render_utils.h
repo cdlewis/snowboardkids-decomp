9128eae0b7f76469de1e90dcf8829acb093bfb68
Chris Lewis <me@chrislewis.au>
2026-08-27 16:05:49 -0400

    Export menu render utility APIs

--- include/game/menu/renderer/menu_render_utils.h
+++ include/game/menu/renderer/menu_render_utils.h
@@ -7,6 +7,7 @@ typedef struct AssetTable AssetTable;
 
 void getAssetTableImageAndPalette(void *asset, u16 index, void **image, void **palette);
 void drawMenuAsciiChar(s16 x, s16 y, u8 character, u16 palette);
+void drawMenuAsciiCharImpl(s16 x, s16 y, u8 character, u16 palette);
 void drawAssetTableSprite(s16 x, s16 y, AssetTable *table, u16 entryIndex);
 void drawPulsingAssetTableSprite(s16 x, s16 y, AssetTable *table, u16 entryIndex);
 void drawAssetTableSpriteWithDefaultPalette(s16 x, s16 y, AssetTable *table, u16 entryIndex);
@@ -27,5 +28,8 @@ Mtx *allocTranslationOnlyFixedMatrix(Mtx *source);
 void setPackedMatrixTranslation(Mtx *matrix, Vec3i *translation);
 void copyPackedMatrixTranslation(Mtx *source, Mtx *dest);
 s32 isPositionNearAnyRaceViewportFocus(Vec3i *position);
+void allocMenuRenderScratchBuffers(void);
+void selectMenuRenderScratchBuffer(s32 bufferIndex);
+void *allocMenuRenderScratch(s32 size);
 
 #endif
