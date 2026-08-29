637b883437c546aa295154b95a7be49b957d3a69
Chris Lewis <me@chrislewis.au>
2026-08-29 01:37:55 -0400

    Clean up race player progress structs

--- include/game/race/player/race_player_progress.h
+++ include/game/race/player/race_player_progress.h
@@ -2,12 +2,16 @@
 #define RACE_PLAYER_PROGRESS_H
 
 #include "common.h"
+#include "game/race/player/race_player_input.h"
 
-struct RacePlayer;
+typedef struct RacePlayerCheckpointEvent {
+    /* 0x00 */ s16 pathFrame;
+    /* 0x02 */ s16 surfaceCueMask;
+} RacePlayerCheckpointEvent;
 
 void updateRacePlayerRankDisplay(void);
 s32 updateRacePlayerSmoothedPathOffset(s32 playerIndex, s32 pathIndex, s32 rankSlot);
 s32 getRacePlayerPathOffset(s32 playerIndex, s32 pathIndex);
-void updateRacePlayerCheckpointEvents(struct RacePlayer *player);
+void updateRacePlayerCheckpointEvents(RacePlayer *player);
 
 #endif
