e5b9d209a3d67d19f080a7f9dd5433a00c2dee3b
Chris Lewis <me@chrislewis.au>
2026-08-30 10:28:22 -0400

    Deduplicate ending credits character actors

--- include/game/ending/ending_credits_linda.h
+++ include/game/ending/ending_credits_linda.h
@@ -1,21 +1,14 @@
 #ifndef ENDING_CREDITS_LINDA_H
 #define ENDING_CREDITS_LINDA_H
 
-#include "common.h"
+#include "game/ending/ending_credits_character.h"
 
-typedef struct EndingCreditsLinda {
-    char pad[0x18];
-    s32 posX;
-    s32 posY;
-    s32 posZ;
-    s16 rotX;
-    s16 rotY;
-    s16 rotZ;
-    u16 timer;
-    u16 animTimer;
-    u16 textureId;
-    u16 paletteId;
-} EndingCreditsLinda;
+typedef struct EndingCreditsLindaActor {
+    /* 0x00 */ EndingCreditsCharacterActor character;
+    /* 0x2C */ u16 animationTimer;
+    /* 0x2E */ u16 textureId;
+    /* 0x30 */ u16 paletteId;
+} EndingCreditsLindaActor;
 
 typedef struct EndingCreditsTumblingSnowboard {
     char pad0[0x10];
@@ -27,56 +20,56 @@ typedef struct EndingCreditsTumblingSnowboard {
     u16 timer;
 } EndingCreditsTumblingSnowboard;
 
-void updateEndingLindaFinalPose(EndingCreditsLinda *arg0);
-void updateEndingLindaStartFinalPose(EndingCreditsLinda *arg0);
-void updateEndingLindaPhase40Prep(EndingCreditsLinda *arg0);
-void updateEndingLindaWaitBeforePhase40Prep(EndingCreditsLinda *arg0);
-void updateEndingLindaWaitPhase3FAnim(EndingCreditsLinda *arg0);
-void updateEndingLindaSpinUntilPhase3F(EndingCreditsLinda *arg0);
-void updateEndingLindaTumbleToPhase3C(EndingCreditsLinda *arg0);
-void updateEndingLindaExitUntilPhase3C(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase3A(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideToCenter(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase38(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftSetPhase36(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase35(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftFromFarRight(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase31(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftSetPhase2E(EndingCreditsLinda *arg0);
-void updateEndingLindaWaitBeforePhase2E(EndingCreditsLinda *arg0);
-void updateEndingLindaRunLeftSetPhase2D(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase2C(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideRightSetPhase28(EndingCreditsLinda *arg0);
-void updateEndingLindaWaitBeforeSlideRight(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftToPose(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideRightAfterPause(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftAfterPause(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideRightToPhase23Wait(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase23(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideRightSetPhase21(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftToFarLeft(EndingCreditsLinda *arg0);
-void updateEndingLindaRepeatAnimUntilPhase20(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase1D(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftSetPhase1C(EndingCreditsLinda *arg0);
-void updateEndingLindaAfterIntroAnim2(EndingCreditsLinda *arg0);
-void updateEndingLindaAfterIntroAnim1(EndingCreditsLinda *arg0);
-void updateEndingLindaWaitBeforePhase1B(EndingCreditsLinda *arg0);
-void updateEndingLindaHopRightToIdle(EndingCreditsLinda *arg0);
-void updateEndingLindaHopRightToPose(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideRightSetPhase1A(EndingCreditsLinda *arg0);
-void updateEndingLindaHandshakeAnimComplete(EndingCreditsLinda *arg0);
-void updateEndingLindaHandshakeLoop(EndingCreditsLinda *arg0);
-void startEndingLindaHandshakeLoop(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase19(EndingCreditsLinda *arg0);
-void updateEndingLindaWaitThenSetPhase18(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftUntilPhase17(EndingCreditsLinda *arg0);
-void updateEndingLindaBlinkThenSlideLeft(EndingCreditsLinda *arg0);
-void updateEndingLindaStartBlinkLoop(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideRightUntilPhase16(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase15(EndingCreditsLinda *arg0);
-void updateEndingLindaSlideLeftSetPhase14(EndingCreditsLinda *arg0);
-void waitEndingLindaPhase13(EndingCreditsLinda *arg0);
-void initEndingCreditsLinda(EndingCreditsLinda *arg0);
+void updateEndingLindaFinalPose(EndingCreditsLindaActor *arg0);
+void updateEndingLindaStartFinalPose(EndingCreditsLindaActor *arg0);
+void updateEndingLindaPhase40Prep(EndingCreditsLindaActor *arg0);
+void updateEndingLindaWaitBeforePhase40Prep(EndingCreditsLindaActor *arg0);
+void updateEndingLindaWaitPhase3FAnim(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSpinUntilPhase3F(EndingCreditsLindaActor *arg0);
+void updateEndingLindaTumbleToPhase3C(EndingCreditsLindaActor *arg0);
+void updateEndingLindaExitUntilPhase3C(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase3A(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideToCenter(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase38(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftSetPhase36(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase35(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftFromFarRight(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase31(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftSetPhase2E(EndingCreditsLindaActor *arg0);
+void updateEndingLindaWaitBeforePhase2E(EndingCreditsLindaActor *arg0);
+void updateEndingLindaRunLeftSetPhase2D(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase2C(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideRightSetPhase28(EndingCreditsLindaActor *arg0);
+void updateEndingLindaWaitBeforeSlideRight(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftToPose(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideRightAfterPause(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftAfterPause(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideRightToPhase23Wait(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase23(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideRightSetPhase21(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftToFarLeft(EndingCreditsLindaActor *arg0);
+void updateEndingLindaRepeatAnimUntilPhase20(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase1D(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftSetPhase1C(EndingCreditsLindaActor *arg0);
+void updateEndingLindaAfterIntroAnim2(EndingCreditsLindaActor *arg0);
+void updateEndingLindaAfterIntroAnim1(EndingCreditsLindaActor *arg0);
+void updateEndingLindaWaitBeforePhase1B(EndingCreditsLindaActor *arg0);
+void updateEndingLindaHopRightToIdle(EndingCreditsLindaActor *arg0);
+void updateEndingLindaHopRightToPose(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideRightSetPhase1A(EndingCreditsLindaActor *arg0);
+void updateEndingLindaHandshakeAnimComplete(EndingCreditsLindaActor *arg0);
+void updateEndingLindaHandshakeLoop(EndingCreditsLindaActor *arg0);
+void startEndingLindaHandshakeLoop(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase19(EndingCreditsLindaActor *arg0);
+void updateEndingLindaWaitThenSetPhase18(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftUntilPhase17(EndingCreditsLindaActor *arg0);
+void updateEndingLindaBlinkThenSlideLeft(EndingCreditsLindaActor *arg0);
+void updateEndingLindaStartBlinkLoop(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideRightUntilPhase16(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase15(EndingCreditsLindaActor *arg0);
+void updateEndingLindaSlideLeftSetPhase14(EndingCreditsLindaActor *arg0);
+void waitEndingLindaPhase13(EndingCreditsLindaActor *arg0);
+void initEndingCreditsLinda(EndingCreditsLindaActor *arg0);
 void spawnEndingCreditsTumblingSnowboard(s32 arg0, s32 arg1, s32 arg2, u16 arg3, u16 arg4, u8 arg5);
 void drawEndingCreditsTumblingSnowboard(EndingCreditsTumblingSnowboard *arg0);
 void updateEndingCreditsTumblingSnowboardWaitForRemove(EndingCreditsTumblingSnowboard *arg0);
--- include/game/ending/ending_credits_tommy.h
+++ include/game/ending/ending_credits_tommy.h
@@ -1,51 +1,38 @@
 #ifndef ENDING_CREDITS_TOMMY_H
 #define ENDING_CREDITS_TOMMY_H
 
-#include "common.h"
-
-typedef struct EndingCreditsTommy {
-    /* 0x00 */ char pad0[0x18];
-    /* 0x18 */ s32 posX;
-    /* 0x1C */ s32 posY;
-    /* 0x20 */ s32 posZ;
-    /* 0x24 */ s16 rotX;
-    /* 0x26 */ s16 rotY;
-    /* 0x28 */ s16 rotZ;
-    /* 0x2A */ u16 timer;
-} EndingCreditsTommy;
+#include "game/ending/ending_credits_character.h"
 
 typedef struct {
-    /* 0x00 */ s32 posX;
-    /* 0x04 */ s32 posY;
-    /* 0x08 */ s32 posZ;
-    /* 0x0C */ s8 unkC;
-    /* 0x0D */ s8 actorId;
+    /* 0x00 */ Vec3i offset;
+    /* 0x0C */ s8 modelPartIndex;
+    /* 0x0D */ s8 characterId;
 } MainMenuSceneActorShadow;
 
 void noopEndingCreditsTommy(void);
-void updateEndingTommyFinalPose(EndingCreditsTommy *arg0);
-void updateEndingTommyStartFinalPose(EndingCreditsTommy *arg0);
-void updateEndingTommyWaitThenFinalPhase(EndingCreditsTommy *arg0);
-void waitEndingTommyPhase3D(EndingCreditsTommy *arg0);
-void updateEndingTommyEnterForPhase3A(EndingCreditsTommy *arg0);
-void waitEndingTommyPhase39(EndingCreditsTommy *arg0);
-void updateEndingTommySlideLeftAfterBurst(EndingCreditsTommy *arg0);
-void updateEndingTommyStartBurstExit(EndingCreditsTommy *arg0);
-void updateEndingTommyWaitBeforeBurstExit(EndingCreditsTommy *arg0);
-void updateEndingTommyWaitBeforeBurst(EndingCreditsTommy *arg0);
-void waitEndingTommyPhase0F(EndingCreditsTommy *arg0);
-void updateEndingTommyStartPhase0CAuras(EndingCreditsTommy *arg0);
-void updateEndingTommyWaitBeforePhase0CAuras(EndingCreditsTommy *arg0);
-void updateEndingTommySlideLeftToPhase0A(EndingCreditsTommy *arg0);
-void updateEndingTommyHopLeftToPhase0A(EndingCreditsTommy *arg0);
-void waitEndingTommyPhase0B(EndingCreditsTommy *arg0);
-void waitEndingTommyPhase08Aura(EndingCreditsTommy *arg0);
-void updateEndingTommyRepeatAnimThenPhase07(EndingCreditsTommy *arg0);
-void updateEndingTommyWaitForPhase06(EndingCreditsTommy *arg0);
-void waitEndingTommyPhase04(EndingCreditsTommy *arg0);
-void updateEndingTommyEnterToCenter(EndingCreditsTommy *arg0);
-void waitEndingTommyPhase01(EndingCreditsTommy *arg0);
-void initEndingCreditsTommy(EndingCreditsTommy *arg0);
+void updateEndingTommyFinalPose(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyStartFinalPose(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyWaitThenFinalPhase(EndingCreditsCharacterActor *arg0);
+void waitEndingTommyPhase3D(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyEnterForPhase3A(EndingCreditsCharacterActor *arg0);
+void waitEndingTommyPhase39(EndingCreditsCharacterActor *arg0);
+void updateEndingTommySlideLeftAfterBurst(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyStartBurstExit(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyWaitBeforeBurstExit(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyWaitBeforeBurst(EndingCreditsCharacterActor *arg0);
+void waitEndingTommyPhase0F(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyStartPhase0CAuras(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyWaitBeforePhase0CAuras(EndingCreditsCharacterActor *arg0);
+void updateEndingTommySlideLeftToPhase0A(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyHopLeftToPhase0A(EndingCreditsCharacterActor *arg0);
+void waitEndingTommyPhase0B(EndingCreditsCharacterActor *arg0);
+void waitEndingTommyPhase08Aura(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyRepeatAnimThenPhase07(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyWaitForPhase06(EndingCreditsCharacterActor *arg0);
+void waitEndingTommyPhase04(EndingCreditsCharacterActor *arg0);
+void updateEndingTommyEnterToCenter(EndingCreditsCharacterActor *arg0);
+void waitEndingTommyPhase01(EndingCreditsCharacterActor *arg0);
+void initEndingCreditsTommy(EndingCreditsCharacterActor *arg0);
 void addEndingActorShadowRenderCallback(MainMenuSceneActorShadow *arg0);
 
 #endif
