bf6c4c6c16eab9b6b8dea44f97a46b517f4629de
Chris Lewis <me@chrislewis.au>
2026-08-30 03:39:52 -0400

    clean header ownership: '[include/game/menu/training/training_course_ui.h,src/menu/training/training_course_ui.c]'

--- include/game/menu/training/training_course_ui.h
+++ include/game/menu/training/training_course_ui.h
@@ -2,8 +2,38 @@
 #define TRAINING_COURSE_UI_H
 
 #include "common.h"
+#include "game/engine/callback_task_scheduler.h"
+#include "game/menu/renderer/menu_renderer.h"
 
-typedef struct TrainingCourseUiActor TrainingCourseUiActor;
+typedef MenuGlyphScript TrainingCourseUiScript;
+
+typedef struct TrainingCourseUiActor {
+    /* 0x00 */ CallbackTaskHeader task;
+    /* 0x18 */ s16 x;
+    /* 0x1A */ s16 y;
+    union {
+        struct {
+            /* 0x1C */ s16 x;
+            /* 0x1E */ s16 y;
+        } textOffset;
+        /* 0x1C */ TrainingCourseUiScript *script;
+        /* 0x1C */ s32 panelHeight;
+    } layout;
+    /* 0x20 */ s16 highlightScale;
+    /* 0x22 */ s16 highlightTimer;
+    union {
+        /* 0x24 */ s16 selectedAction;
+        struct {
+            /* 0x24 */ u8 visibleGlyphCount;
+            /* 0x25 */ u8 pad25;
+        } script;
+    } state;
+    /* 0x26 */ u16 glyphPalette;
+    /* 0x28 */ u16 glyphTimer;
+    /* 0x2A */ u8 scriptState;
+    /* 0x2B */ u8 confirmBlinkTimer;
+    /* 0x2C */ u8 keepVisibleAfterConfirm;
+} TrainingCourseUiActor;
 
 void initTrainingCourseLessonEndMenu(TrainingCourseUiActor *arg0);
 void initTrainingCourseOpeningDialog(TrainingCourseUiActor *arg0);
--- src/menu/training/training_course_ui.c
+++ src/menu/training/training_course_ui.c
@@ -11,36 +11,6 @@
 
 #define TRAINING_COURSE_UI_TEXTURE_HANDLE (gAssetHandles[0x2A])
 
-typedef MenuGlyphScript TrainingCourseUiScript;
-
-struct TrainingCourseUiActor {
-    /* 0x00 */ CallbackTaskHeader task;
-    /* 0x18 */ s16 x;
-    /* 0x1A */ s16 y;
-    union {
-        struct {
-            /* 0x1C */ s16 x;
-            /* 0x1E */ s16 y;
-        } textOffset;
-        /* 0x1C */ TrainingCourseUiScript *script;
-        /* 0x1C */ s32 panelHeight;
-    } layout;
-    /* 0x20 */ s16 highlightScale;
-    /* 0x22 */ s16 highlightTimer;
-    union {
-        /* 0x24 */ s16 selectedAction;
-        struct {
-            /* 0x24 */ u8 visibleGlyphCount;
-            /* 0x25 */ u8 pad25;
-        } script;
-    } state;
-    /* 0x26 */ u16 glyphPalette;
-    /* 0x28 */ u16 glyphTimer;
-    /* 0x2A */ u8 scriptState;
-    /* 0x2B */ u8 confirmBlinkTimer;
-    /* 0x2C */ u8 keepVisibleAfterConfirm;
-};
-
 typedef struct {
     TrainingCourseUiScript lessonTitleText[0xB3];
     s16 lessonTitleXPositions[0xD];
