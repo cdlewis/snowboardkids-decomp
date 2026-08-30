51003d1e5cdc423d1a03daef56ac7c2e617c5986
Chris Lewis <me@chrislewis.au>
2026-08-30 04:52:07 -0400

    Fix looping sound declaration ownership

--- src/race/player/race_player_update.c
+++ src/race/player/race_player_update.c
@@ -60,7 +60,6 @@ typedef union {
     u64 align;
 } RaceSpawnX;
 
-extern void enqueuePlayerLoopingPositionalSoundRequest(s32, Vec3i *, s32, s32, f32, s16);
 extern u8 gMainMenuModeSelection;
 extern s8 gRacePlayerCount;
 extern u8 gRaceUpdatePaused;
