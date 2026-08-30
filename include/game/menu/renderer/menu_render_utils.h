b3cf4d2bfc74f429ffaec1d961c98d89047716fd
Chris Lewis <me@chrislewis.au>
2026-08-30 05:03:51 -0400

    Fix race UI sprite prototype ownership

--- include/game/menu/renderer/menu_render_utils.h
+++ include/game/menu/renderer/menu_render_utils.h
@@ -22,6 +22,7 @@ void drawMenuAsciiCharImpl(s16 x, s16 y, u8 character, u16 palette);
 void drawMenuAsciiCharLegacy(s16 x, s16 y, volatile s32 character, u16 palette);
 void drawMenuAsciiTextDefaultScale(s16 x, s16 y, const char *text, u16 palette);
 void drawAssetTableSprite(s16 x, s16 y, AssetTable *table, u16 entryIndex);
+void drawAssetTableSprite8bpp(s16 x, s16 y, AssetTable *table, u16 entryIndex);
 void drawAssetTableSpriteWideIndex(s16 x, s16 y, AssetTable *table, s32 entryIndex);
 void drawMenuFillRectangle(s16 x, s16 y, s16 width, s16 height, u8 red, u8 green, u8 blue);
 void drawPulsingAssetTableSprite(s16 x, s16 y, AssetTable *table, u16 entryIndex);
